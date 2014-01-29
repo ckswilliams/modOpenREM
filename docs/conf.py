75602a644b026bdb64d7a58e2ed43b4bbfd7f653
Ed McDonagh <ed@mcdonagh.org.uk>
2014-01-29 22:33:03 +0000

    Moved docs files to root of repository

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 
 basepath = os.path.dirname(__file__)
-projectpath = os.path.abspath(os.path.join(basepath, ".."))
+projectpath = os.path.abspath(os.path.join(basepath, "..", "openrem"))
 sys.path.insert(0, projectpath)
 
 # If extensions (or modules to document with autodoc) are in another directory,
