1acb48d53914bd99d24efb12984841d1a11ac6f9
Ed McDonagh <ed@mcdonagh.org.uk>
2014-02-01 16:27:09 +0000

    Changing version number to 0.3.3-b1 for test build

--- distribute/setup.py
+++ distribute/setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name='OpenREM',
-    version='0.3.2',
+    version='0.3.3-b1',
     packages=['openrem'],
     include_package_data=True,
     install_requires = requires,
