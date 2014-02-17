c22e7a0a6b05b4bb1b2030cb93c3cc0bda90e889
Ed McDonagh <ed@mcdonagh.org.uk>
2014-02-17 16:28:43 +0000

    Adding docstrings and documentation pages for the patient size csv import routine. Fixes #31.

--- openrem/remapp/extractors/ptsizecsv2db.py
+++ openrem/remapp/extractors/ptsizecsv2db.py
@@ -1,4 +1,32 @@
-#!/usr/local/bin/python
+#    OpenREM - Radiation Exposure Monitoring tools for the physicist
+#    Copyright (C) 2012,2013  The Royal Marsden NHS Foundation Trust
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    Additional permission under section 7 of GPLv3:
+#    You shall not make any use of the name of The Royal Marsden NHS
+#    Foundation trust in connection with this Program in any press or 
+#    other public announcement without the prior written consent of 
+#    The Royal Marsden NHS Foundation Trust.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+..  module:: ptsizecsv2db.
+    :synopsis: Use to import height and weight data from csv file to existing studies in the database.
+
+..  moduleauthor:: Ed McDonagh
+
+"""
 
 def _patientstudymoduleattributes(exam, height, weight): # C.7.2.2
     patientatt = exam.patient_study_module_attributes_set.get()
@@ -32,14 +60,23 @@ def _ptsizeinsert(accno,height,weight,siuid):
 
     
 def csv2db(*args, **kwargs):
-    """ Import patient height and weight data from csv RIS exports
+    """ Import patient height and weight data from csv RIS exports. Can be called from ``openrem_ptsizecsv.py`` script
         
-    Arguments:
-    filename : relative or absolute path to csv file.
-    
-    Limitations:
-    Currently expects a column named 'PACS_SPS_ID' containing the accession
-    number as held by the database, and two columns named 'HEIGHT' and 'WEIGHT'.
+    :param --si-uid: Use Study Instance UID instead of Accession Number. Short form -s.
+    :type --si-uid: bool
+    :param csvfile: relative or absolute path to csv file
+    :type csvfile: str
+    :param id: Accession number column header or header if -u or --si-uid is set. Quote if necessary.
+    :type id: str
+    :param height: Patient height column header. Create if necessary, quote if necessary.
+    :type height: str
+    :param weight: Patient weight column header. Create if necessary, quote if necessary.
+    :type weight: str
+
+    Example::
+        
+        openrem_ptsizecsv.py -s MyRISExport.csv StudyInstanceUID HEIGHT weight
+
     """
 
     import os, csv
