cb545301e3d00fe58313e4e79e7d11ea9e593536
Ed McDonagh <ed@mcdonagh.org.uk>
2014-02-25 16:48:15 +0000

    Bumped version number to 0.3.7 and updated CHANGES file for #35, #36, #38, #40.

--- distribute/setup.py
+++ distribute/setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name='OpenREM',
-    version='0.3.7-a1',
+    version='0.3.7',
     packages=['openrem'],
     include_package_data=True,
     install_requires = requires,
