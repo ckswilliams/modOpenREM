0cf3c6cf6048ce380cf38d39b125a43c646e772b
Ed McDonagh <ed@mcdonagh.org.uk>
2014-03-04 17:23:43 +0000

    Bumping version number to 0.3.8-b2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name='OpenREM',
-    version='0.3.8-b1',
+    version='0.3.8-b2',
     packages=['openrem'],
     include_package_data=True,
     install_requires = requires,
