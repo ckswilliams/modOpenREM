e0b71d6781bd9e1c228ea76699fd8ecb99ee3f5a
Ed McDonagh <ed@mcdonagh.org.uk>
2014-03-08 23:44:25 +0000

    Bumped version number to 0.3.9.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name='OpenREM',
-    version='0.3.9-a1',
+    version='0.3.9',
     packages=['openrem'],
     include_package_data=True,
     install_requires = requires,
