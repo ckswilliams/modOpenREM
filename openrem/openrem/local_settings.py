705dca295cf9c251c92fd9298537f6dcd7093ff1
Ed McDonagh <ed@mcdonagh.org.uk>
2014-03-31 21:58:41 +0100

    Added notice regarding database settings being moved to local_settings.py. Moved STATIC_ROOT and MEDIA_ROOT into local_settings with appropriate notices. Refs #65.

--- openrem/openrem/local_settings.py
+++ openrem/openrem/local_settings.py
@@ -12,3 +12,14 @@ DATABASES = {
     }
 }
 
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
--- openrem/openrem/settings.py
+++ openrem/openrem/settings.py
@@ -14,6 +14,11 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'django.core.context_processors.request',
 )
 
+# **********************************************************************
+#
+# Database settings have been moved to local_settings.py
+#
+# **********************************************************************
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -38,20 +43,18 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+#
+# MEDIA_ROOT filepath has been moved to local_settings.py
+#
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+#
+# STATIC_ROOT filepath has been moved to local_settings.py
+#
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
