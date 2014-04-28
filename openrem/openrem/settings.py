704caa538463047631093206afba54ef2ccb12fb
Ed McDonagh <ed@mcdonagh.org.uk>
2014-04-28 21:50:07 +0100

    Added celery.py, edited openrem/__init__.py and openrem/settings as per the first steps tutorial and django tutorial. Haven't tested yet (or added any taksks). Refs #19.

--- openrem/openrem/settings.py
+++ openrem/openrem/settings.py
@@ -3,6 +3,21 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+from __future__ import absolute_import
+# ^^^ The above is required if you want to import from the celery
+# library.  If you don't have this then `from celery.schedules import`
+# becomes `proj.celery.schedules` in Python 2.x since it allows
+# for relative imports by default.
+
+# Celery settings
+
+BROKER_URL = 'amqp://guest:guest@localhost//'
+CELERY_RESULT_BACKEND = 'amqp'
+
+
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json']
 
 
 import os
