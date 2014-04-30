8c4c8f469f4d959528d16d2e7f381b7d4b24a165
Ed McDonagh <ed@mcdonagh.org.uk>
2014-04-30 12:45:45 +0100

    Progress bar task now works - Celery server must be running (celery -A openrem worker -l info) and CELERY_TASK_SERIALIZER = 'json' and CELERY_RESULT_SERIALIZER = 'json' had to be added to settings to force use of json instead of pickle, which was being blocked by the security. Refs #19.

--- openrem/openrem/settings.py
+++ openrem/openrem/settings.py
@@ -19,6 +19,8 @@ CELERY_RESULT_BACKEND = 'amqp'
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
 CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 
 import os
