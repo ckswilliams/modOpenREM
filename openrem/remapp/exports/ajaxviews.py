9ed4f61be3d7b65dfad92ed1c8b9ad8140c9c79b
Ed McDonagh <ed@mcdonagh.org.uk>
2014-05-19 08:22:43 +0100

    Removed the redundant sample code.

--- openrem/remapp/exports/ajaxviews.py
+++ openrem/remapp/exports/ajaxviews.py
@@ -25,24 +25,6 @@ def ct_csv(request):
 
 
 @csrf_exempt
-def do_CT_csv(request):
-    from remapp.exports.exportcsv import exportCT2excel
-    from remapp.exports.exportcsv import getQueryFilters
-    
-    data = 'Fail'
-    if request.is_ajax():
-        query_filters = getQueryFilters(request)
-        job = exportCT2excel.delay(query_filters)
-        request.session['task_id'] = job.id
-        data = job.id
-    else:
-        data = "Not ajax request!"
-        
-    json_data = json.dumps(data)
-
-    return HttpResponse(json_data, content_type='application/json')
-
-@csrf_exempt
 def poll_state(request):
     """ A view to report the progress to the user """
     from celery.result import AsyncResult
