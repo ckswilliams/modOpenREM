8ee2510a7b7d1ec362b33e979face49291ad41af
Ed McDonagh <ed@mcdonagh.org.uk>
2014-06-16 09:47:03 +0100

    Added the new Exports table and Size_upload table to the admin interface. Refs #19 and #21.

--- openrem/remapp/admin.py
+++ openrem/remapp/admin.py
@@ -13,7 +13,8 @@ from remapp.models import (General_study_module_attributes,
     Patient_study_module_attributes, Content_item_descriptions,
     Ct_radiation_dose, Ct_accumulated_dose_data,
     Ct_irradiation_event_data, Scanning_length,
-    Ct_dose_check_details, Ct_xray_source_parameters)
+    Ct_dose_check_details, Ct_xray_source_parameters,
+    Exports, Size_upload)
 
 from django.contrib import admin
 
@@ -50,4 +51,5 @@ admin.site.register(Ct_irradiation_event_data)
 admin.site.register(Scanning_length)
 admin.site.register(Ct_dose_check_details)
 admin.site.register(Ct_xray_source_parameters)
-
+admin.site.register(Exports)
+admin.site.register(Size_upload)
