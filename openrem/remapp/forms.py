6f7b664f6999d52b1341eded7da645b0cff55c68
Ed McDonagh <ed@mcdonagh.org.uk>
2014-06-17 22:27:06 +0100

    Adding form to collect header information. Refs #21.

--- openrem/remapp/forms.py
+++ openrem/remapp/forms.py
@@ -4,3 +4,11 @@ class SizeUploadForm(forms.Form):
     sizefile = forms.FileField(
         label='Select a file'
     )
+
+class SizeHeadersForm(forms.Form):
+    height_field = forms.CharField()
+    weight_field = forms.CharField()
+    id_field = forms.CharField()
+    ID_TYPES = (("si-uid", "Study instance UID"), ("acc-no", "Accession Number"))
+    id_type = forms.ChoiceField(widget=forms.RadioSelect, choices=ID_TYPES)
+    
