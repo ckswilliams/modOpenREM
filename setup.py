1de1802c579a099e24505d29d3da7f45af2174ce
Ed McDonagh <ed@mcdonagh.org.uk>
2014-09-21 15:24:14 +0100

    Bumping version number to 0.4.3b6Celery. Adapted release docs to indicate that this is specifically to keep a version I can install if anything happens with the Celery bug filed as https://github.com/celery/celery/issues/2247. Changed setup.py to require django == 1.6 rather than django >= 1.5, as otherwise 1.7 will install which doesn't work.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 requires = [
-    'django >= 1.5',
+    'django == 1.6',
     'django-filter',
     'pytz >= 0a',
     'humanize',
@@ -21,7 +21,7 @@ requires = [
 
 setup(
     name='OpenREM',
-    version='0.4.3b5',
+    version='0.4.3b6Celery',
     packages=['openrem'],
     include_package_data=True,
     install_requires = requires,
