4948d9039462821cc267f3c4942eaf91cff26030
David Platten <david.platten@ngh.nhs.uk>
2014-12-01 11:07:41 +0000

    Added acquisitionsPerHourInWeekdays to dx_histogram_list_filter so that the workload charts work when the user has clicked through a histogram plot. Helps with issue #157.

--- openrem/remapp/views.py
+++ openrem/remapp/views.py
@@ -174,6 +174,16 @@ def dx_histogram_list_filter(request):
             dapValues = f.qs.exclude(projection_xray_radiation_dose__irradiation_event_xray_data__dose_area_product__isnull=True).filter(projection_xray_radiation_dose__irradiation_event_xray_data__acquisition_protocol=protocol.get('projection_xray_radiation_dose__irradiation_event_xray_data__acquisition_protocol')).exclude(Q(projection_xray_radiation_dose__irradiation_event_xray_data__dose_area_product__isnull=True)).values_list('projection_xray_radiation_dose__irradiation_event_xray_data__dose_area_product', flat=True)
             acquisitionHistogramData[idx][0], acquisitionHistogramData[idx][1] = np.histogram([float(x)*1000000 for x in dapValues], bins=20)
 
+        acquisitionsPerHourInWeekdays = [[0 for x in range(24)] for x in range(7)]
+        for day in range(7):
+            acquisitionTimesOnThisWeekday = f.qs.filter(study_date__week_day=day+1).values('study_time')
+            if acquisitionTimesOnThisWeekday:
+                for hour in range(24):
+                    try:
+                        acquisitionsPerHourInWeekdays[day][hour] = (acquisitionTimesOnThisWeekday.filter(Q(study_time__gte = str(hour)+':00') & Q(study_time__lte = str(hour)+':59')).values('study_time').annotate(num_acq_this_hour = Count('study_instance_uid')))[0].values()[0]
+                    except:
+                        acquisitionsPerHourInWeekdays[day][hour] = 0
+
     try:
         vers = pkg_resources.require("openrem")[0].version
     except:
@@ -190,7 +200,8 @@ def dx_histogram_list_filter(request):
             'remapp/dxfiltered.html',
             {'filter': f, 'admin':admin,
              'acquisitionSummary': acquisitionSummary,
-             'acquisitionHistogramData': acquisitionHistogramData},
+             'acquisitionHistogramData': acquisitionHistogramData,
+             'acquisitionsPerHourInWeekdays': acquisitionsPerHourInWeekdays},
             context_instance=RequestContext(request)
             )
     else:
