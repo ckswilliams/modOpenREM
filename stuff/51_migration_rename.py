8ce41df46f33ec50ca2a6dd8a00d989574cae7d3
Ed McDonagh <ed@mcdonagh.org.uk>
2015-01-03 22:55:16 +0000

    Added start of a backwards migration. Refs #172, #62, #55, #154. Need to remember foreign keys

--- stuff/51_migration_rename.py
+++ stuff/51_migration_rename.py
@@ -8,9 +8,16 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting model 'Ct_dose_check_details'
+        # Renaming model from 'Ct_dose_check_details' to 'CtDoseCheckDetails'
         db.rename_table(u'remapp_ct_dose_check_details', u'remapp_ctdosecheckdetails')
         if not db.dry_run:
             orm['contenttypes.contenttype'].objects.filter(
                 app_label='remapp', model='ct_dose_check_details').update(model='ctdosecheckdetails')
 
+
+    def backwards(self, orm):
+        # Renaming model from 'CtDoseCheckDetails' to 'Ct_dose_check_details'
+        db.rename_table(u'remapp_ctdosecheckdetails', u'remapp_ct_dose_check_details')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='ctdosecheckdetails').update(model='ct_dose_check_details')
