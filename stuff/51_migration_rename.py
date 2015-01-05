4652597d3b303925420b3e60aac4a0870975e722
Ed McDonagh <ed@mcdonagh.org.uk>
2015-01-05 08:43:25 +0000

    Added TODO to remind me to sort out the foreign fields at the end. Added CtRadiationDose in both directions.

--- stuff/51_migration_rename.py
+++ stuff/51_migration_rename.py
@@ -5,6 +5,9 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 
+# TODO: Run through all ForeignKey fields in models and change them in both directions
+
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
@@ -14,6 +17,12 @@ class Migration(SchemaMigration):
             orm['contenttypes.contenttype'].objects.filter(
                 app_label='remapp', model='ct_dose_check_details').update(model='ctdosecheckdetails')
 
+        # Renaming model from 'Ct_radiation_dose' to 'CtRadiationDose'
+        db.rename_table(u'remapp_ct_radiation_dose', u'remapp_ctradiationdose')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='ct_radiation_dose').update(model='ctradiationdose')
+
 
     def backwards(self, orm):
         # Renaming model from 'CtDoseCheckDetails' to 'Ct_dose_check_details'
@@ -21,3 +30,9 @@ class Migration(SchemaMigration):
         if not db.dry_run:
             orm['contenttypes.contenttype'].objects.filter(
                 app_label='remapp', model='ctdosecheckdetails').update(model='ct_dose_check_details')
+
+        # Renaming model from 'CtRadiationDose' to 'Ct_radiation_dose'
+        db.rename_table(u'remapp_ctradiationdose', u'remapp_ct_radiation_dose')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='ctradiationdose').update(model='ct_radiation_dose')
