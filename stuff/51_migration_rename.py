bd131199e727cbc1fab28f2e6eee3bafc254b669
Ed McDonagh <ed@mcdonagh.org.uk>
2015-01-05 09:33:38 +0000

    Added AccumXRayDose and PatientStudyModuleAttr in both directions.

--- stuff/51_migration_rename.py
+++ stuff/51_migration_rename.py
@@ -23,6 +23,18 @@ class Migration(SchemaMigration):
             orm['contenttypes.contenttype'].objects.filter(
                 app_label='remapp', model='ct_radiation_dose').update(model='ctradiationdose')
 
+        # Renaming model from 'Accumulated_projection_xray_dose' to 'AccumXRayDose'
+        db.rename_table(u'remapp_accumulated_projection_xray_dose', u'remapp_accumxraydose')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='accumulated_projection_xray_dose').update(model='accumxraydose')
+
+        # Renaming model from 'Patient_study_module_attributes' to 'PatientStudyModuleAttr'
+        db.rename_table(u'remapp_patient_study_module_attributes', u'remapp_patientstudymoduleattr')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='patient_study_module_attributes').update(model='patientstudymoduleattr')
+
 
     def backwards(self, orm):
         # Renaming model from 'CtDoseCheckDetails' to 'Ct_dose_check_details'
@@ -36,3 +48,15 @@ class Migration(SchemaMigration):
         if not db.dry_run:
             orm['contenttypes.contenttype'].objects.filter(
                 app_label='remapp', model='ctradiationdose').update(model='ct_radiation_dose')
+
+        # Renaming model from 'AccumXRayDose' to 'Accumulated_projection_xray_dose'
+        db.rename_table(u'remapp_accumxraydose', u'remapp_accumulated_projection_xray_dose')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='accumxraydose').update(model='accumulated_projection_xray_dose')
+
+        # Renaming model from 'PatientStudyModuleAttr' to 'Patient_study_module_attributes'
+        db.rename_table(u'remapp_patientstudymoduleattr', u'remapp_patient_study_module_attributes')
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(
+                app_label='remapp', model='patientstudymoduleattr').update(model='patient_study_module_attributes')
