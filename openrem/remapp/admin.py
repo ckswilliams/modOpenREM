1174a0bc2c30f1a1a956975b9b41613d6e2b8303
David Platten <david.platten@ngh.nhs.uk>
2015-03-15 19:45:31 +0000

    Added the userprofile data to the admin page so that it can be changed. Followed instructions here: https://docs.djangoproject.com/en/1.7/topics/auth/customizing/. Helps with issue #140.

--- openrem/remapp/admin.py
+++ openrem/remapp/admin.py
@@ -53,3 +53,19 @@ admin.site.register(CtDoseCheckDetails)
 admin.site.register(CtXRaySourceParameters)
 admin.site.register(Exports)
 admin.site.register(SizeUpload)
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from remapp.models import UserProfile
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'userprofile'
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
