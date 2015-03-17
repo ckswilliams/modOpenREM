3ecfc1b512d430d5185a60b5122460ebb87a335a
David Platten <david.platten@ngh.nhs.uk>
2015-03-17 15:43:50 +0000

    Taken out some in-line JavaScript in ctfiltered.html and dxfiltered.html and put into external files. Helps with issue #191.

---
+++ openrem/remapp/static/js/formatDate.js
@@ -0,0 +1,9 @@
+function formatDate(d) {
+  var dd = d.getDate();
+  if ( dd < 10 ) dd = '0' + dd;
+  var mm = d.getMonth()+1;
+  if ( mm < 10 ) mm = '0' + mm;
+  var yyyy = d.getFullYear();
+  return yyyy+'-'+mm+'-'+dd;
+}
+
