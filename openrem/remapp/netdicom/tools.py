70d74df533c8836301ea7daee425a13287b4c8fe
Ed McDonagh <ed@mcdonagh.org.uk>
2015-03-24 20:37:40 +0000

    Added more thoughts on UUID creation.

--- openrem/remapp/netdicom/tools.py
+++ openrem/remapp/netdicom/tools.py
@@ -2,9 +2,23 @@
 # Provided by Medical Connections https://www.medicalconnections.co.uk/FreeUID
 
 
-# ImplementationUID 1.2.826.0.1.3680043.9.5224.1.0.6.0
-# = 1.2.826.0.1.3680043.9.5224.1.version number
+# ImplementationUID 1.2.826.0.1.3680043.9.5224.1.0.6.0.1
+# = 1.2.826.0.1.3680043.9.5224.1.versionnumber.betanumber
 
 # UID root for objects
 # = 1.2.826.0.1.3680043.9.5224.2.machine-root.machineID.numberperimage
 # where numberperimage  might consist of yyyymmddhhmmssss.number
+
+# Also applied for iana root, which will be along the lines of:
+# 1.3.6.1.4.1.45556.  with the 45556 number being that or higher.
+
+# pydicom has a UID generator of the form:
+# root + mac + pid + second + microsecond, eg
+# 1.2.826.0.1.3680043.9.5224.2.+8796759879378+15483+44+908342
+# 1.2.826.0.1.3680043.9.5224.2.87967598793781548344908342
+# which is 54 characters but process ID could be longer.
+
+# 1.3.5.1.4.1.45556.1.2.879675987937815483yyyymmddssssssss
+# would be 55 characters - process ID could be longer.
+# Includes an extra 1. after the root UID to enable future use for
+# anthing else.
