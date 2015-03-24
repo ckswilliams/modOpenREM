39b36362a24f6030ed695b72cdcef6bc63e1850b
Ed McDonagh <ed@mcdonagh.org.uk>
2015-03-24 08:29:04 +0000

    Adding netdicom folder and file with just UID root information in currently.

---
+++ openrem/remapp/netdicom/tools.py
@@ -0,0 +1,10 @@
+# OpenREM root UID: 1.2.826.0.1.3680043.9.5224.
+# Provided by Medical Connections https://www.medicalconnections.co.uk/FreeUID
+
+
+# ImplementationUID 1.2.826.0.1.3680043.9.5224.1.0.6.0
+# = 1.2.826.0.1.3680043.9.5224.1.version number
+
+# UID root for objects
+# = 1.2.826.0.1.3680043.9.5224.2.machine-root.machineID.numberperimage
+# where numberperimage  might consist of yyyymmddhhmmssss.number
