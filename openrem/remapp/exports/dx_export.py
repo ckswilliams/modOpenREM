ceebcfe3eb4cf16b5ce3d05cda8dcecea056e9a8
Ed McDonagh <ed@mcdonagh.org.uk>
2015-03-26 14:03:16 +0000

    Adding entrance exposure at RP to export of DX. Not in common use, but is used by a GE example. Refs #196

--- openrem/remapp/exports/dx_export.py
+++ openrem/remapp/exports/dx_export.py
@@ -291,6 +291,7 @@ def dxxlsx(filterdict):
         'Exposure index',
         'Relative x-ray exposure',
         'DAP (cGy.cm^2)',
+        'Entrance exposure at RP'
         ]
         
     # Generate list of protocols in queryset and create worksheets for each
@@ -353,9 +354,10 @@ def dxxlsx(filterdict):
             'E' + str(h+1) + ' Exposure index',
             'E' + str(h+1) + ' Relative x-ray exposure',
             'E' + str(h+1) + ' DAP (cGy.cm^2)',
+            'E' + str(h+1) + ' DAP (mGy)',
             ]
     wsalldata.write_row('A1', alldataheaders)
-    numcolumns = (22 * max_events['projectionxrayradiationdose__accumxraydose__accumintegratedprojradiogdose__total_number_of_radiographic_frames__max']) + 14 - 1
+    numcolumns = (23 * max_events['projectionxrayradiationdose__accumxraydose__accumintegratedprojradiogdose__total_number_of_radiographic_frames__max']) + 14 - 1
     numrows = e.count()
     wsalldata.autofilter(0,0,numrows,numcolumns)
 
@@ -393,7 +395,8 @@ def dxxlsx(filterdict):
                 str(s.irradeventxraydetectordata_set.get().exposure_index),
                 str(s.irradeventxraydetectordata_set.get().relative_xray_exposure),
                 str(s.convert_gym2_to_cgycm2()),
-                ]
+                str(s.entrance_exposure_at_rp),
+            ]
 
         wsalldata.write_row(row+1,0, examdata)
         
@@ -437,7 +440,8 @@ def dxxlsx(filterdict):
                 str(s.irradeventxraydetectordata_set.get().exposure_index),
                 str(s.irradeventxraydetectordata_set.get().relative_xray_exposure),
                 str(s.convert_gym2_to_cgycm2()),
-                ]
+                str(s.entrance_exposure_at_rp),
+            ]
 
             sheetlist[tabtext]['sheet'].write_row(sheetlist[tabtext]['count'],0,examdata)
 
