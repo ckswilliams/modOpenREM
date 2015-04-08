996aae6e2e5a309201b46bebab91bf07a02725e2
David Platten <david.platten@ngh.nhs.uk>
2015-04-08 21:29:53 +0100

    Improved behaviour of the chart. Title of drilldown now changes appropriately. Changes back on drillup. Tooltip information is correct, and continues to work once drilled back up (didn't before). Need to supress the display of the secondary y-axis on drilldown. References issue 202.

--- openrem/remapp/static/js/ctChartDLPandCTDIperAcquisition.js
+++ openrem/remapp/static/js/ctChartDLPandCTDIperAcquisition.js
@@ -7,13 +7,52 @@ var tooltipData = [2];
 var chartAcqDLPandCTDI = new Highcharts.Chart({
         chart: {
             type: 'column',
-            renderTo: 'histogramPlotDLPandCTDIdiv'
+            renderTo: 'histogramPlotDLPandCTDIdiv',
+            events: {
+                drilldown: function(e) {
+                    tooltipData[0] = protocolNames[e.point.x];
+                    tooltipData[1] = e.point.x;
+                    var this_series_title = e.point.series.index == 0 ? ' DLP' : ' CTDI<sub>vol</sub>';
+                    chartAcqDLPandCTDI.setTitle({ text: drilldownTitle + e.point.name + this_series_title}, { text: '(n = ' + seriesDataN[e.point.x] +')' });
+                    chartAcqDLPandCTDI.yAxis[0].setTitle({text:'Number'});
+                    chartAcqDLPandCTDI.xAxis[0].setTitle({text:e.point.series.index == 0 ? 'DLP range (mGy.cm)' : 'CTDI<sub>vol</sub> range (mGy)'});
+                    chartAcqDLPandCTDI.xAxis[0].setCategories([], true);
+                    chartAcqDLPandCTDI.tooltip.options.formatter = function(args) {
+                        return 'n = ' + this.y.toFixed(0);
+                    }
+                },
+                drillup: function(e) {
+                    chartAcqDLPandCTDI.setTitle({ text: defaultTitle }, { text: '' });
+                    chartAcqDLPandCTDI.yAxis[0].setTitle({text:'DLP (mGy.cm)'});
+                    chartAcqDLPandCTDI.yAxis[1].setTitle({text:'CTDI<sub>vol</sub> (mGy)'});
+                    chartAcqDLPandCTDI.xAxis[0].setTitle({text:''});
+                    chartAcqDLPandCTDI.xAxis[0].setCategories(protocolNames, true);
+                    chartAcqDLPandCTDI.xAxis[0].update({labels:{rotation:90}});
+                    chartAcqDLPandCTDI.tooltip.options.formatter = function(args) {
+                        var this_point_index = this.series.data.indexOf( this.point );
+                        var this_series_index = 0;//this.series.index;
+                        var that_series_index = 1;//this.series.index == 0 ? 1 : 0; // assuming 2 series
+                        var this_series = args.chart.series[this_series_index];
+                        var that_series = args.chart.series[that_series_index];
+                        var this_point = this_series.data[this_point_index];
+                        var that_point = that_series.data[this_point_index];
+                        var this_series_label = ' mGy.cm DLP';//this.series.index == 0 ? ' mGy.cm DLP' : ' mGy CTDI<sub>vol</sub>';
+                        var that_series_label = ' mGy CTDI<sub>vol</sub>';//this.series.index == 0 ? ' mGy CTDI<sub>vol</sub>' : ' mGy.cm DLP';
+                        return this.point.name +
+                            '<br/>' + this_point.y.toFixed(1) + this_series_label +
+                            '<br/>' + that_point.y.toFixed(1) + that_series_label;
+                    }
+                }
+            }
         },
         title: {
             useHTML: true,
             text: 'Mean DLP and CTDI<sub>vol</sub> per acquisition protocol'
         },
         xAxis: {
+            title: {
+                useHTML: true
+            },
             categories: protocolNames,
             labels: {
                 useHTML: true,
@@ -72,7 +111,7 @@ var chartAcqDLPandCTDI = new Highcharts.Chart({
             floating: true,
             borderWidth: 0,
             x: 0,
-            y: 30
+            y: 40
         }
     });
 });
