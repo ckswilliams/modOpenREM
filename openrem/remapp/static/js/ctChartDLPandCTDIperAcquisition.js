dd7438e6fc723de8516f8f3adcb7f1de2f1ae7da
David Platten <david.platten@ngh.nhs.uk>
2015-05-14 18:16:27 +0100

    Legend in combined DLP and CTDI histogram now correct. Title of the same histogram now removed. Still need to fix the links from the histogram tooltips when both DLP and CTDI histogram being displayed. References issue #228

--- openrem/remapp/static/js/ctChartDLPandCTDIperAcquisition.js
+++ openrem/remapp/static/js/ctChartDLPandCTDIperAcquisition.js
@@ -7,6 +7,8 @@ $(function () {
     var index;
     for (index = 0; index < seriesDrilldownCTDI.length; ++index) {
         seriesDrilldownCTDI[index].xAxis = 1;
+        seriesDrilldownCTDI[index].name = seriesDrilldownCTDI[index].name + ' CTDI';
+        seriesDrilldown[index].name = seriesDrilldown[index].name + ' DLP';
     }
 
     var chartAcqDLPandCTDI = new Highcharts.Chart({
@@ -17,17 +19,15 @@ $(function () {
                 drilldown: function(e) {
                     tooltipData[0] = protocolNames[e.point.x];
                     tooltipData[1] = e.point.x;
-                    parentSeriesIndex = e.point.series.index;
-                    parentSeriesName = e.point.series.name;
+                    var parentSeriesName = e.point.series.name;
                     var this_series_title = parentSeriesName.indexOf('DLP') != -1 ? ' DLP' : ' CTDI<sub>vol</sub>';
-                    chartAcqDLPandCTDI.setTitle({ text: drilldownTitle + e.point.name + this_series_title}, { text: '(n = ' + seriesDataN[e.point.x] +')' });
+                    chartAcqDLPandCTDI.setTitle({ text: '' });
                     chartAcqDLPandCTDI.yAxis[0].setTitle({text:'Number'});
-                    chartAcqDLPandCTDI.xAxis[0].setTitle({text:parentSeriesName.indexOf('DLP') != -1 ? 'DLP range (mGy.cm)' : 'CTDI<sub>vol</sub> range (mGy)'});
                     if (parentSeriesName.indexOf('DLP') != -1) {
                         chartAcqDLPandCTDI.xAxis[0].setTitle({text:'DLP range (mGy.cm)'});
                     }
                     if (parentSeriesName.indexOf('CTDI') != -1) {
-                        chartAcqDLPandCTDI.xAxis[1].setTitle({text:'CTDI range (mGy)'});
+                        chartAcqDLPandCTDI.xAxis[1].setTitle({text:'CTDI<sub>vol</sub> range (mGy)'});
                     }
                     chartAcqDLPandCTDI.xAxis[0].setCategories([], true);
                     chartAcqDLPandCTDI.tooltip.options.formatter = function(args) {
@@ -60,14 +60,10 @@ $(function () {
                     chartAcqDLPandCTDI.xAxis[0].update({labels:{rotation:90}});
                     chartAcqDLPandCTDI.tooltip.options.formatter = function(args) {
                         var this_point_index = this.series.data.indexOf( this.point );
-                        var this_series_index = 0;//this.series.index;
-                        var that_series_index = 1;//this.series.index == 0 ? 1 : 0; // assuming 2 series
-                        var this_series = args.chart.series[this_series_index];
-                        var that_series = args.chart.series[that_series_index];
-                        var this_point = this_series.data[this_point_index];
-                        var that_point = that_series.data[this_point_index];
-                        var this_series_label = ' mGy.cm DLP';//this.series.index == 0 ? ' mGy.cm DLP' : ' mGy CTDI<sub>vol</sub>';
-                        var that_series_label = ' mGy CTDI<sub>vol</sub>';//this.series.index == 0 ? ' mGy CTDI<sub>vol</sub>' : ' mGy.cm DLP';
+                        var this_point = args.chart.series[0].data[this_point_index];
+                        var that_point = args.chart.series[1].data[this_point_index];
+                        var this_series_label = ' mGy.cm DLP';
+                        var that_series_label = ' mGy CTDI<sub>vol</sub>';
                         return this.point.name +
                             '<br/>' + this_point.y.toFixed(1) + this_series_label +
                             '<br/>' + that_point.y.toFixed(1) + that_series_label +
@@ -160,12 +156,12 @@ $(function () {
         },
         legend: {
             useHTML: true,
-            align: 'center',
+            align: 'left',
             verticalAlign: 'top',
             floating: true,
             borderWidth: 0,
-            x: 0,
-            y: 40
+            x: 100,
+            y: 120
         }
     });
 });
