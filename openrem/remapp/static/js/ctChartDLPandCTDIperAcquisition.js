cc3c75f2d53a2d00dfcb46a4baa51155f03e93fb
dplatten <dplatten@hotmail.com>
2015-05-19 15:41:54 +0100

    Reformatted the layout of this file. No functional changes.

--- openrem/remapp/static/js/ctChartDLPandCTDIperAcquisition.js
+++ openrem/remapp/static/js/ctChartDLPandCTDIperAcquisition.js
@@ -1,7 +1,7 @@
 $(function () {
 
     var drilldownTitle = 'Histogram of ';
-    var defaultTitle   = 'Mean DLP and CTDI<sub>vol</sub> per acquisition protocol';
+    var defaultTitle = 'Mean DLP and CTDI<sub>vol</sub> per acquisition protocol';
     var tooltipData = [2];
 
     var index;
@@ -16,22 +16,22 @@ $(function () {
             type: 'column',
             renderTo: 'histogramPlotDLPandCTDIdiv',
             events: {
-                drilldown: function(e) {
+                drilldown: function (e) {
                     tooltipData[0] = protocolNames[e.point.x];
                     tooltipData[1] = e.point.x;
                     var parentSeriesName = e.point.series.name;
                     var this_series_title = parentSeriesName.indexOf('DLP') != -1 ? ' DLP' : ' CTDI<sub>vol</sub>';
-                    chartAcqDLPandCTDI.setTitle({ text: '' });
-                    chartAcqDLPandCTDI.yAxis[0].setTitle({text:'Number'});
+                    chartAcqDLPandCTDI.setTitle({text: ''});
+                    chartAcqDLPandCTDI.yAxis[0].setTitle({text: 'Number'});
                     if (parentSeriesName.indexOf('DLP') != -1) {
-                        chartAcqDLPandCTDI.xAxis[0].setTitle({text:'DLP range (mGy.cm)'});
+                        chartAcqDLPandCTDI.xAxis[0].setTitle({text: 'DLP range (mGy.cm)'});
                     }
                     if (parentSeriesName.indexOf('CTDI') != -1) {
-                        chartAcqDLPandCTDI.xAxis[1].setTitle({text:'CTDI<sub>vol</sub> range (mGy)'});
+                        chartAcqDLPandCTDI.xAxis[1].setTitle({text: 'CTDI<sub>vol</sub> range (mGy)'});
                     }
                     chartAcqDLPandCTDI.xAxis[0].setCategories([], true);
-                    chartAcqDLPandCTDI.tooltip.options.formatter = function(args) {
-                        if (this.series.name.indexOf('DLP') != -1) {  // Now working
+                    chartAcqDLPandCTDI.tooltip.options.formatter = function (args) {
+                        if (this.series.name.indexOf('DLP') != -1) {
                             var linkText = 'acquisition_dlp_min=' + protocolBins[tooltipData[1]][this.x] + '&acquisition_dlp_max=' + protocolBins[tooltipData[1]][this.x + 1] + '&acquisition_protocol=' + tooltipData[0];
                             var returnValue = '<table style="text-align: center"><tr><td>' + this.y.toFixed(0) + ' exposures</td></tr><tr><td><a href="/openrem/ct/hist/?acquisitionhist=1&' + linkText + tooltipFiltersAcq + '">Click to view</a></td></tr></table>';
                         }
@@ -50,26 +50,26 @@ $(function () {
                         }
                     });
                 },
-                drillup: function(e) {
-                    chartAcqDLPandCTDI.setTitle({ text: defaultTitle }, { text: '' });
-                    chartAcqDLPandCTDI.yAxis[0].setTitle({text:'DLP (mGy.cm)'});
-                    chartAcqDLPandCTDI.yAxis[1].setTitle({text:'CTDI<sub>vol</sub> (mGy)'});
-                    chartAcqDLPandCTDI.xAxis[0].setTitle({text:''});
-                    chartAcqDLPandCTDI.xAxis[1].setTitle({text:''});
+                drillup: function (e) {
+                    chartAcqDLPandCTDI.setTitle({text: defaultTitle}, {text: ''});
+                    chartAcqDLPandCTDI.yAxis[0].setTitle({text: 'DLP (mGy.cm)'});
+                    chartAcqDLPandCTDI.yAxis[1].setTitle({text: 'CTDI<sub>vol</sub> (mGy)'});
+                    chartAcqDLPandCTDI.xAxis[0].setTitle({text: ''});
+                    chartAcqDLPandCTDI.xAxis[1].setTitle({text: ''});
                     chartAcqDLPandCTDI.xAxis[0].setCategories(protocolNames, true);
-                    chartAcqDLPandCTDI.xAxis[0].update({labels:{rotation:90}});
-                    chartAcqDLPandCTDI.tooltip.options.formatter = function(args) {
-						var this_point_index = this.series.data.indexOf( this.point );
-						if (this.series.name.indexOf('DLP') != -1) {
-							var this_series_label = ' mGy.cm DLP';
-							var this_series = args.chart.series[0];
-						}
-						else {
-							var this_series_label = ' mGy CTDI<sub>vol</sub>';
-							var this_series = args.chart.series[1];
-						}
-						var this_point = this_series.data[this_point_index];
-						return this.point.name + '<br/>' + this_point.y.toFixed(1) + this_series_label + '<br/>(n = ' + seriesDataN[this_point_index] + ')';
+                    chartAcqDLPandCTDI.xAxis[0].update({labels: {rotation: 90}});
+                    chartAcqDLPandCTDI.tooltip.options.formatter = function (args) {
+                        var this_point_index = this.series.data.indexOf(this.point);
+                        if (this.series.name.indexOf('DLP') != -1) {
+                            var this_series_label = ' mGy.cm DLP';
+                            var this_series = args.chart.series[0];
+                        }
+                        else {
+                            var this_series_label = ' mGy CTDI<sub>vol</sub>';
+                            var this_series = args.chart.series[1];
+                        }
+                        var this_point = this_series.data[this_point_index];
+                        return this.point.name + '<br/>' + this_point.y.toFixed(1) + this_series_label + '<br/>(n = ' + seriesDataN[this_point_index] + ')';
                     };
                     chartAcqDLPandCTDI.yAxis[1].update({
                         labels: {
@@ -94,7 +94,7 @@ $(function () {
             type: 'category',
             labels: {
                 useHTML: true,
-                rotation:90
+                rotation: 90
             }
         }, {
             title: {
@@ -104,7 +104,7 @@ $(function () {
             opposite: true,
             labels: {
                 useHTML: true,
-                rotation:90
+                rotation: 90
             }
         }],
         yAxis: [{
@@ -121,16 +121,16 @@ $(function () {
         }],
         tooltip: {
             useHTML: true,
-            formatter: function(args) {
-				var this_point_index = this.series.data.indexOf( this.point );
-				if (this.series.name.indexOf('DLP') != -1) {
-					var this_series_label = ' mGy.cm DLP';
-					var this_series = args.chart.series[0];
-				}
-				else {
-					var this_series_label = ' mGy CTDI<sub>vol</sub>';
-					var this_series = args.chart.series[1];
-				}
+            formatter: function (args) {
+                var this_point_index = this.series.data.indexOf(this.point);
+                if (this.series.name.indexOf('DLP') != -1) {
+                    var this_series_label = ' mGy.cm DLP';
+                    var this_series = args.chart.series[0];
+                }
+                else {
+                    var this_series_label = ' mGy CTDI<sub>vol</sub>';
+                    var this_series = args.chart.series[1];
+                }
                 var this_point = this_series.data[this_point_index];
                 return this.point.name + '<br/>' + this_point.y.toFixed(1) + this_series_label + '<br/>(n = ' + seriesDataN[this_point_index] + ')';
             }
