ceab3adc8d3cca84b9ce756671974e4a5e022686
Ed McDonagh <ed@mcdonagh.org.uk>
2015-06-08 21:46:26 +0100

    Commented out form as the built-in form-handling should deal with it. Refs #207

--- openrem/remapp/forms.py
+++ openrem/remapp/forms.py
@@ -21,11 +21,11 @@ class SizeUploadForm(forms.Form):
     )
 
 
-class DicomStoreConfigForm(forms.Form):
-    """Form for setting the DICOM Store SCP details
-    """
-    aetitle_field = forms.CharField(max_length=16)
-    port_field = forms.IntegerField(max_value=65535)
+#class DicomStoreConfigForm(forms.Form):
+#    """Form for setting the DICOM Store SCP details
+#    """
+#    aetitle_field = forms.CharField(max_length=16)
+#    port_field = forms.IntegerField(max_value=65535)
 
 
 class DicomQRConfigForm(forms.Form):
