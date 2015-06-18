894b21dac6c5407bf083877937a82310ab67f7fa
David Platten <david.platten@ngh.nhs.uk>
2015-06-18 19:20:28 +0100

    Combined migration file creates fields and does the data migration, but doesn't create the new database median functions. References issue #214.

--- stuff/00xx_add_median_function.py
+++ stuff/00xx_add_median_function.py
@@ -9,7 +9,10 @@ from datetime import datetime
 
 def populate_study_workload_chart_time(self, orm):
 
-    for studyData in orm.GeneralStudyModuleAttr.objects.all():
+    test = self.get_model('remapp', 'GeneralStudyModuleAttr')
+
+#    for studyData in orm.GeneralStudyModuleAttr.objects.all():
+    for studyData in test.objects.all():
         studyDate = datetime.date(datetime(1900,1,1,0,0,0,0))
         studyTime = studyData.study_time
         if studyTime:
@@ -26,41 +29,43 @@ class Migration(migrations.Migration):
         ('remapp', '0001_initial'),
     ]
 
-    if 'postgresql' in project_settings.DATABASES['default']['ENGINE']:
-        operations = [
-            migrations.RunPython(populate_study_workload_chart_time),
-            migrations.RunSQL([
-                "CREATE FUNCTION _final_median(anyarray) RETURNS NUMERIC AS $$"
-                "  WITH q AS"
-                "  ("
-                "     SELECT val"
-                "     FROM UNNEST($1) val"
-                "     WHERE val IS NOT NULL"
-                "     ORDER BY 1"
-                "  ),"
-                "  cnt AS"
-                "  ("
-                "    SELECT COUNT(*) AS c FROM q"
-                "  )"
-                "  SELECT AVG(val * 10000000000.0)"
-                "  FROM"
-                "  ("
-                "    SELECT val FROM q"
-                "    LIMIT  2 - MOD((SELECT c FROM cnt), 2)"
-                "    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1, 0)"
-                "  ) q2;"
-                "$$ LANGUAGE SQL IMMUTABLE;"
-                "CREATE AGGREGATE median(anyelement) ("
-                "  SFUNC=array_append,"
-                "  STYPE=anyarray,"
-                "  FINALFUNC=_final_median,"
-                "  INITCOND='{}'"
-                ");",
-                "DROP AGGREGATE median(anyelement);"
-                "DROP FUNCTION _final_median(anyarray);"]
-            ),
-        ]
-    else:
-        operations = [
-            migrations.RunPython(populate_study_workload_chart_time),
-        ]
+
+
+    operations = [
+        migrations.AddField('generalstudymoduleattr', 'study_workload_chart_time', models.DateTimeField(blank=True, null=True)),
+        migrations.RunPython(populate_study_workload_chart_time),
+        migrations.AddField('userprofile', 'median_available', models.BooleanField(default=False)),
+        migrations.AddField('userprofile', 'plotAverageChoice', models.CharField(default='mean', max_length=6)),
+        migrations.AddField('userprofile', 'plotCTRequestMeanDLP', models.BooleanField(default=False)),
+        migrations.AddField('userprofile', 'plotCTRequestFreq', models.BooleanField(default=False)),
+        migrations.RunSQL([
+            "CREATE FUNCTION _final_median(anyarray) RETURNS NUMERIC AS $$"
+            "  WITH q AS"
+            "  ("
+            "     SELECT val"
+            "     FROM UNNEST($1) val"
+            "     WHERE val IS NOT NULL"
+            "     ORDER BY 1"
+            "  ),"
+            "  cnt AS"
+            "  ("
+            "    SELECT COUNT(*) AS c FROM q"
+            "  )"
+            "  SELECT AVG(val * 10000000000.0)"
+            "  FROM"
+            "  ("
+            "    SELECT val FROM q"
+            "    LIMIT  2 - MOD((SELECT c FROM cnt), 2)"
+            "    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1, 0)"
+            "  ) q2;"
+            "$$ LANGUAGE SQL IMMUTABLE;"
+            "CREATE AGGREGATE median(anyelement) ("
+            "  SFUNC=array_append,"
+            "  STYPE=anyarray,"
+            "  FINALFUNC=_final_median,"
+            "  INITCOND='{}'"
+            ");",
+            "DROP AGGREGATE median(anyelement);"
+            "DROP FUNCTION _final_median(anyarray);"]
+        ),
+    ]
