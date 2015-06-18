d8c15b56f2bba2a6254bfbce8d19d4fe0664ec35
dplatten <dplatten@hotmail.com>
2015-06-18 10:53:23 +0100

    Added a check for PostgreSQL use in the median migration file so that the median functions are only executed if PostgreSQL is being used. Untested. References issue #241.

--- stuff/00xx_add_median_function.py
+++ stuff/00xx_add_median_function.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 from django.conf import settings
-
+from openremproject import settings as project_settings
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -11,35 +11,38 @@ class Migration(migrations.Migration):
         ('remapp', '0001_initial'),
     ]
 
-    operations = [
-        migrations.RunSQL([
-            "CREATE FUNCTION _final_median(anyarray) RETURNS NUMERIC AS $$"
-            "  WITH q AS"
-            "  ("
-            "     SELECT val"
-            "     FROM UNNEST($1) val"
-            "     WHERE val IS NOT NULL"
-            "     ORDER BY 1"
-            "  ),"
-            "  cnt AS"
-            "  ("
-            "    SELECT COUNT(*) AS c FROM q"
-            "  )"
-            "  SELECT AVG(val * 10000000000.0)"
-            "  FROM"
-            "  ("
-            "    SELECT val FROM q"
-            "    LIMIT  2 - MOD((SELECT c FROM cnt), 2)"
-            "    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1, 0)"
-            "  ) q2;"
-            "$$ LANGUAGE SQL IMMUTABLE;"
-            "CREATE AGGREGATE median(anyelement) ("
-            "  SFUNC=array_append,"
-            "  STYPE=anyarray,"
-            "  FINALFUNC=_final_median,"
-            "  INITCOND='{}'"
-            ");",
-            "DROP AGGREGATE median(anyelement);"
-            "DROP FUNCTION _final_median(anyarray);"]
-        ),
-    ]
+    if 'postgresql' in project_settings.DATABASES['default']['ENGINE']:
+        operations = [
+            migrations.RunSQL([
+                "CREATE FUNCTION _final_median(anyarray) RETURNS NUMERIC AS $$"
+                "  WITH q AS"
+                "  ("
+                "     SELECT val"
+                "     FROM UNNEST($1) val"
+                "     WHERE val IS NOT NULL"
+                "     ORDER BY 1"
+                "  ),"
+                "  cnt AS"
+                "  ("
+                "    SELECT COUNT(*) AS c FROM q"
+                "  )"
+                "  SELECT AVG(val * 10000000000.0)"
+                "  FROM"
+                "  ("
+                "    SELECT val FROM q"
+                "    LIMIT  2 - MOD((SELECT c FROM cnt), 2)"
+                "    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1, 0)"
+                "  ) q2;"
+                "$$ LANGUAGE SQL IMMUTABLE;"
+                "CREATE AGGREGATE median(anyelement) ("
+                "  SFUNC=array_append,"
+                "  STYPE=anyarray,"
+                "  FINALFUNC=_final_median,"
+                "  INITCOND='{}'"
+                ");",
+                "DROP AGGREGATE median(anyelement);"
+                "DROP FUNCTION _final_median(anyarray);"]
+            ),
+        ]
+    else:
+        operations = []
