1117f16b4c449a23fffd2323fd57d5bb03c30498
dplatten <dplatten@hotmail.com>
2015-06-19 16:47:22 +0100

    Fixed import error with 0.7.0b2. Fixes issue #244.

--- openrem/remapp/extractors/dx.py
+++ openrem/remapp/extractors/dx.py
@@ -36,6 +36,7 @@
 
 import os
 import sys
+import django
 
 # setup django/OpenREM
 basepath = os.path.dirname(__file__)
@@ -43,6 +44,7 @@ projectpath = os.path.abspath(os.path.join(basepath, "..", ".."))
 if projectpath not in sys.path:
     sys.path.insert(1,projectpath)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'openremproject.settings'
+django.setup()
 
 from celery import shared_task
 
