a14e479aa37d415452e375bc7701f606c099a09e
Ed McDonagh <ed@mcdonagh.org.uk>
2015-07-03 22:51:33 +0100

    Moved the basic function into a function.

--- openrem/remapp/netdicom/qrscu.py
+++ openrem/remapp/netdicom/qrscu.py
@@ -21,27 +21,6 @@ from dicom.UID import ExplicitVRLittleEndian, ImplicitVRLittleEndian, ExplicitVR
 import netdicom
 import tempfile
 
-# parse commandline
-parser = argparse.ArgumentParser(description='storage SCU example')
-parser.add_argument('remotehost')
-parser.add_argument('remoteport', type=int)
-parser.add_argument('-aet', help='calling AE title', default='OPENREM')
-parser.add_argument('-aec', help='called AE title', default='STOREDCMTK')
-parser.add_argument('-implicit', action='store_true', help='negociate implicit transfer syntax only', default=False)
-parser.add_argument('-explicit', action='store_true', help='negociate explicit transfer syntax only', default=False)
-
-args = parser.parse_args()
-
-if args.implicit:
-    ts = [ImplicitVRLittleEndian]
-elif args.explicit:
-    ts = [ExplicitVRLittleEndian]
-else:
-    ts = [
-        ExplicitVRLittleEndian, 
-        ImplicitVRLittleEndian, 
-        ExplicitVRBigEndian
-        ]
 
 # call back
 def OnAssociateResponse(association):
@@ -52,7 +31,7 @@ def OnAssociateRequest(association):
     print "Association resquested"
     return True
 
-def _moveStudy(d):
+def _moveStudy(MyAE, RemoteAE, d):
     assocMove = MyAE.RequestAssociation(RemoteAE)
     print "Move association requested"
     print "d is {0}".format(d)
@@ -64,7 +43,7 @@ def _moveStudy(d):
 
 
 
-def _querySeriesCT(d2):
+def _querySeriesCT(MyAE, RemoteAE, d2):
     d2.QueryRetrieveLevel = "SERIES"
     d2.SeriesDescription = ''
     d2.SeriesNumber = ''
@@ -86,7 +65,7 @@ def _querySeriesCT(d2):
             # Not sure if they will be SR are series level but CT at study level?
             # If they are, send C-Move request
             print "C-Move request for series with modality type SR"
-            _moveStudy(series[1])
+            _moveStudy(MyAE, RemoteAE, series[1])
             continue
         seNum = str(series[1].SeriesNumber)
         if (seNum == '502') or (seNum == '998') or (seNum == '990') or (seNum == '9001'):
@@ -94,13 +73,13 @@ def _querySeriesCT(d2):
             # Added 9001 for Toshiba XA based on a sample of 1
             # Then Send a C-Move request for that series
             print "C-Move request for series with number {0}".format(seNum)
-            _moveStudy(series[1])
+            _moveStudy(MyAE, RemoteAE, series[1])
             continue
         try:
             if series[1].SeriesDescription == 'Dose Info':
                 # Get Philips Dose Info series - not SR but enough information in the header
                 print "C-Move request for series with description {0}".format(series[1].SeriesDescription)
-                _moveStudy(series[1])
+                _moveStudy(MyAE, RemoteAE, series[1])
                 continue
         except AttributeError:
             # Try an image level find?
@@ -110,84 +89,109 @@ def _querySeriesCT(d2):
     assoc2.Release(0)
     print "Released series association"
 
-# create application entity with Find and Move SOP classes as SCU
-MyAE = AE(args.aet, 0, [StudyRootFindSOPClass,
-                             StudyRootMoveSOPClass,
-                             VerificationSOPClass], [], ts)
-MyAE.OnAssociateResponse = OnAssociateResponse
-MyAE.OnAssociateRequest = OnAssociateRequest
-# MyAE.OnReceiveStore = OnReceiveStore
-MyAE.start()
+def qrscu(*args, **kwargs):
+    import argparse
+
+    # parse commandline
+    parser = argparse.ArgumentParser(description='storage SCU example')
+    parser.add_argument('remotehost')
+    parser.add_argument('remoteport', type=int)
+    parser.add_argument('-aet', help='calling AE title', default='OPENREM')
+    parser.add_argument('-aec', help='called AE title', default='STOREDCMTK')
+    parser.add_argument('-implicit', action='store_true', help='negociate implicit transfer syntax only', default=False)
+    parser.add_argument('-explicit', action='store_true', help='negociate explicit transfer syntax only', default=False)
+
+    args = parser.parse_args()
+
+    if args.implicit:
+        ts = [ImplicitVRLittleEndian]
+    elif args.explicit:
+        ts = [ExplicitVRLittleEndian]
+    else:
+        ts = [
+            ExplicitVRLittleEndian,
+            ImplicitVRLittleEndian,
+            ExplicitVRBigEndian
+            ]
+    # create application entity with Find and Move SOP classes as SCU
+    MyAE = AE(args.aet, 0, [StudyRootFindSOPClass,
+                                 StudyRootMoveSOPClass,
+                                 VerificationSOPClass], [], ts)
+    MyAE.OnAssociateResponse = OnAssociateResponse
+    MyAE.OnAssociateRequest = OnAssociateRequest
+    # MyAE.OnReceiveStore = OnReceiveStore
+    MyAE.start()
+
+    # remote application entity
+    RemoteAE = dict(Address=args.remotehost, Port=args.remoteport, AET=args.aec)
+
+    # create association with remote AE
+    print "Request association"
+    assoc = MyAE.RequestAssociation(RemoteAE)
+
+    # perform a DICOM ECHO
+    print "DICOM Echo ... ",
+    st = assoc.VerificationSOPClass.SCU(1)
+    print 'done with status "%s"' % st
+
+    print "DICOM FindSCU ... ",
+    d = Dataset()
+    d.QueryRetrieveLevel = "STUDY"
+    d.PatientID = ''
+    d.SOPInstanceUID = ''
+    d.Modality = ''
+    d.StudyDescription = ''
+    d.StudyInstanceUID = ''
+    d.StudyDate = ''
+
+    st = assoc.StudyRootFindSOPClass.SCU(d, 1)
+    # print 'done with status "%s"' % st
+
+    responses = True
+    rspno = 0
+
+    for ss in st:
+        if not ss[1]:
+            continue
+        rspno += 1
+        print "Response {0}".format(rspno)
+        if ('CT' in ss[1].Modality) or ('PT' in ss[1].Modality):
+            # new query for series level information
+            print "Starting a series level query for modality type {0}".format(ss[1].Modality)
+            _querySeriesCT(MyAE, RemoteAE, ss[1])
+            continue
+        if ('DX' in ss[1].Modality) or ('CR' in ss[1].Modality):
+            # get everything
+            print "Getting a study with modality type {0}".format(ss[1].Modality)
+            _moveStudy(MyAE, RemoteAE, ss[1])
+            continue
+        if 'MG' in ss[1].Modality:
+            # get everything
+            print "Getting a study with modality type {0}".format(ss[1].Modality)
+            _moveStudy(MyAE, RemoteAE, ss[1])
+            continue
+        if 'SR' in ss[1].Modality:
+            # get it - you may as well
+            print "Getting a study with modality type {0}".format(ss[1].Modality)
+            _moveStudy(MyAE, RemoteAE, ss[1])
+            continue
+        if ('RF' in ss[1].Modality) or ('XA' in ss[1].Modality):
+            # Don't know if you need both...
+            # Get series level information to look for SR
+            print "Starting a series level query for modality type {0}".format(ss[1].Modality)
+            _querySeriesCT(MyAE, RemoteAE, ss[1])
+        print "I got here"
+
+    print "Release association"
+    assoc.Release(0)
+
+    # done
+    MyAE.Quit()
 
 
-# remote application entity
-RemoteAE = dict(Address=args.remotehost, Port=args.remoteport, AET=args.aec)
-
-# create association with remote AE
-print "Request association"
-assoc = MyAE.RequestAssociation(RemoteAE)
-
-
-# perform a DICOM ECHO
-print "DICOM Echo ... ",
-st = assoc.VerificationSOPClass.SCU(1)
-print 'done with status "%s"' % st
-
-print "DICOM FindSCU ... ",
-d = Dataset()
-d.QueryRetrieveLevel = "STUDY"
-d.PatientID = ''
-d.SOPInstanceUID = ''
-d.Modality = ''
-d.StudyDescription = ''
-d.StudyInstanceUID = ''
-d.StudyDate = ''
-
-st = assoc.StudyRootFindSOPClass.SCU(d, 1)
-# print 'done with status "%s"' % st
-
-responses = True
-rspno = 0
-
-for ss in st:
-    if not ss[1]:
-        continue
-    rspno += 1
-    print "Response {0}".format(rspno)
-    if ('CT' in ss[1].Modality) or ('PT' in ss[1].Modality):
-        # new query for series level information
-        print "Starting a series level query for modality type {0}".format(ss[1].Modality)
-        _querySeriesCT(ss[1])
-        continue
-    if ('DX' in ss[1].Modality) or ('CR' in ss[1].Modality):
-        # get everything
-        print "Getting a study with modality type {0}".format(ss[1].Modality)
-        _moveStudy(ss[1])
-        continue
-    if 'MG' in ss[1].Modality:
-        # get everything
-        print "Getting a study with modality type {0}".format(ss[1].Modality)
-        _moveStudy(ss[1])
-        continue
-    if 'SR' in ss[1].Modality:
-        # get it - you may as well
-        print "Getting a study with modality type {0}".format(ss[1].Modality)
-        _moveStudy(ss[1])
-        continue
-    if ('RF' in ss[1].Modality) or ('XA' in ss[1].Modality):
-        # Don't know if you need both...
-        # Get series level information to look for SR
-        print "Starting a series level query for modality type {0}".format(ss[1].Modality)
-        _querySeriesCT(ss[1])
-    print "I got here"
-
-
-
-print "Release association"
-assoc.Release(0)
-
-# done
-MyAE.Quit()
+if __name__ == "__main__":
+    import sys
+    sys.exit(qrscu())
 
 
 # (0008, 0018) SOP Instance UID                    UI:
