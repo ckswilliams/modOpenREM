f6d50131a26a1ac6b81adf041d7d5e79ad41ddee
Ed McDonagh <ed@mcdonagh.org.uk>
2015-07-09 22:27:30 +0100

    Still basic ajax, but it works.

--- openrem/remapp/static/js/dicom.js
+++ openrem/remapp/static/js/dicom.js
@@ -1,20 +1,22 @@
-$('#main-menu a').click(function (event) {
-    // Prevents the default behavior which is
-    // to load the page synchronously
-    event.preventDefault();
+$(document).ready(function(){
+    $('#main-menu a').click(function (event) {
+        // Prevents the default behavior which is
+        // to load the page synchronously
+        event.preventDefault();
 
-    $('#main-ajax').load(this.href + ' #sharemenu *', function (data, status) {
-        $bar = $('#notification-bar');
+        $('#main-ajax').load(this.href + ' #sharemenu *', function (data, status) {
+            $bar = $('#notification-bar');
 
-        if (status === 'success') {
-            $bar.text('The page has been successfully loaded.');
-        } else {
-            $bar.text('An error occurred.');
-        }
+            if (status === 'success') {
+                $bar.text('The page has been successfully loaded.');
+            } else {
+                $bar.text('An error occurred.');
+            }
 
-        $bar
-            .slideDown('normal')
-            .delay(2000)
-            .slideUp('fast');
+            $bar
+                .slideDown('normal')
+                .delay(2000)
+                .slideUp('fast');
+        });
     });
 });
