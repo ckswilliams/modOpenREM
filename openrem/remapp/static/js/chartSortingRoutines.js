2de3abe29018b443b5f3f12ae1c4a8643788d785
dplatten <dplatten@hotmail.com>
2015-07-15 17:34:19 +0100

    Made sorting work for CT plot of mean or median DLP and CTDI. References issue #248.

--- openrem/remapp/static/js/chartSortingRoutines.js
+++ openrem/remapp/static/js/chartSortingRoutines.js
@@ -1,6 +1,6 @@
 function seriesSort(chartContainer, p, d) {
     var chart = $(chartContainer).highcharts();
-    if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "object") {
+    if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "Object" || chart.series[0].chart.drilldownLevels.length == 0) {
         bubbleSort(chart.series[0].data, p, d);
         rebuildSeries(chartContainer);
     }
@@ -8,10 +8,20 @@ function seriesSort(chartContainer, p, d) {
 
 function twoSeriesSort(chartContainer, p, d) {
     var chart = $(chartContainer).highcharts();
-    if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "object") {
+    if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "Object" || chart.series[0].chart.drilldownLevels.length == 0) {
         bubbleSort(chart.series[0].data, p, d);
         rebuildTwoSeries(chartContainer);
         bubbleSort(chart.series[1].data, 'x', 1);
+        chart.redraw({ duration: 1000 });
+    }
+}
+
+function twoSeriesSortB(chartContainer, p, d) {
+    var chart = $(chartContainer).highcharts();
+    if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "Object" || chart.series[0].chart.drilldownLevels.length == 0) {
+        bubbleSort(chart.series[1].data, p, d);
+        rebuildTwoSeriesB(chartContainer);
+        bubbleSort(chart.series[0].data, 'x', 1);
         chart.redraw({ duration: 1000 });
     }
 }
@@ -69,6 +79,49 @@ function rebuildTwoSeries(chartContainer) {
                 newData.name = chart.series[1].data[i].name;
                 newData.freq = chart.series[1].data[i].freq;
                 chart.series[1].data[i].update(newData, false);
+                found = true;
+            }
+            else {
+                j++;
+            }
+        }
+    }
+}
+
+function rebuildTwoSeriesB(chartContainer) {
+    var chart = $(chartContainer).highcharts();
+    var newData = {};
+    var newCategories = [];
+    var i = 0;
+
+    for (i = 0; i < chart.series[1].data.length; i++) {
+        newData.index = chart.series[1].data[i].index;
+        newData.x = i;
+        newData.y = chart.series[1].data[i].y;
+        newData.category = chart.series[1].data[i].category;
+        newData.drilldown = chart.series[1].data[i].drilldown;
+        newData.name = chart.series[1].data[i].name;
+        newData.freq = chart.series[1].data[i].freq;
+        chart.series[1].data[i].update(newData, false);
+        newCategories.push(chart.series[1].data[i].name);
+    }
+    chart.xAxis[0].categories = newCategories;
+
+    for (i = 0; i < chart.series[0].data.length; i++) {
+        var currentIndex = chart.series[0].data[i].index;
+        var found = false;
+
+        var j = 0;
+        while (found == false) {
+            if (chart.series[1].data[j].index == currentIndex) {
+                newData.index = i;
+                newData.x = chart.series[1].data[j].x;
+                newData.y = chart.series[0].data[i].y;
+                newData.category = chart.series[0].data[i].category;
+                newData.drilldown = chart.series[0].data[i].drilldown;
+                newData.name = chart.series[0].data[i].name;
+                newData.freq = chart.series[0].data[i].freq;
+                chart.series[0].data[i].update(newData, false);
                 found = true;
             }
             else {
