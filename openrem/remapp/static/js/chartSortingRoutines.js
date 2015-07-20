d214efb25b219f32f350f8439d8ff146ca295844
David Platten <david.platten@ngh.nhs.uk>
2015-07-20 20:59:50 +0100

    New sorting method applied to DLP+CTDI mean and median plot. Will check that these all work OK on a real database copy before changing method for radiographic plots. References issue #248.

--- openrem/remapp/static/js/chartSortingRoutines.js
+++ openrem/remapp/static/js/chartSortingRoutines.js
@@ -24,32 +24,6 @@ function twoSeriesSort(chartContainer, chartData, chartData2, p, d, s) {
 
 // chartContainer is the div that holds the HighChart; p is the property to sort on; d is the direction of sort: 1 for
 // ascending, anything else for descending; s is the index of the series to sort.
-function fourSeriesSortOld(chartContainer, p, d, s) {
-    var chart = $(chartContainer).highcharts();
-    if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "Object" || chart.series[0].chart.drilldownLevels.length == 0) {
-        if (s == 0) {
-            s2 = 1; s3 = 2; s4 = 3;
-        }
-        else if (s == 1) {
-            s2 = 0; s3 = 2; s4 = 3;
-        }
-        else if (s == 2) {
-            s2 = 0; s3 = 1; s4 = 3;
-        }
-        else {
-            s2 = 0; s3 = 1; s4 = 2;
-        }
-        bubbleSort(chart.series[s].data, p, d);
-        rebuildFourSeries(chartContainer, s);
-        bubbleSort(chart.series[s2].data, 'x', 1);
-        bubbleSort(chart.series[s3].data, 'x', 1);
-        bubbleSort(chart.series[s4].data, 'x', 1);
-        chart.redraw({ duration: 1000 });
-    }
-}
-
-// chartContainer is the div that holds the HighChart; p is the property to sort on; d is the direction of sort: 1 for
-// ascending, anything else for descending; s is the index of the series to sort.
 function fourSeriesSort(chartContainer, chartData, chartData2, chartData3, chartData4, p, d, s) {
     var chart = $(chartContainer).highcharts();
     if(typeof chart.series[0].chart.drilldownLevels == "undefined" || typeof chart.series[0].chart.series[0].drilldownLevel == "Object" || chart.series[0].chart.drilldownLevels.length == 0) {
@@ -128,94 +102,6 @@ function rebuildTwoSeries(chartContainer, chartData, chartData2, s) {
                     category: point.category,
                     tooltip: point.tooltip
                 }, false);
-                found = true;
-            }
-            else {
-                j++;
-            }
-        }
-    }
-}
-
-// chartContainer is the div that holds the HighChart; s is the index of the series to sort.
-function rebuildFourSeriesOld(chartContainer, s) {
-    var s2 = (s == 0 ? 1 : 0);
-    var chart = $(chartContainer).highcharts();
-    var newData = {};
-    var newCategories = [];
-    var i = 0;
-
-    for (i = 0; i < chart.series[s].data.length; i++) {
-        newData.index = chart.series[s].data[i].index;
-        newData.x = i;
-        newData.y = chart.series[s].data[i].y;
-        newData.category = chart.series[s].data[i].category;
-        newData.drilldown = chart.series[s].data[i].drilldown;
-        newData.name = chart.series[s].data[i].name;
-        newData.freq = chart.series[s].data[i].freq;
-        newData.tooltip = chart.series[s].data[i].tooltip;
-        newCategories.push(chart.series[s].data[i].name);
-        chart.series[s].data[i].update(newData, false);
-    }
-    chart.xAxis[0].categories = newCategories;
-
-    for (i = 0; i < chart.series[s2].data.length; i++) {
-        var currentIndex = chart.series[s2].data[i].index;
-        var found = false;
-        var j = 0;
-        while (found == false) {
-            if (chart.series[s].data[j].index == currentIndex) {
-                newData.index = i;
-                newData.x = chart.series[s].data[j].x;
-                newData.y = chart.series[s2].data[i].y;
-                newData.category = chart.series[s2].data[i].category;
-                newData.drilldown = chart.series[s2].data[i].drilldown;
-                newData.name = chart.series[s2].data[i].name;
-                newData.freq = chart.series[s2].data[i].freq;
-                newData.tooltip = chart.series[s2].data[i].tooltip;
-                chart.series[s2].data[i].update(newData, false);
-                found = true;
-            }
-            else {
-                j++;
-            }
-        }
-
-        var currentIndex = chart.series[s3].data[i].index;
-        var found = false;
-        var j = 0;
-        while (found == false) {
-            if (chart.series[s].data[j].index == currentIndex) {
-                newData.index = i;
-                newData.x = chart.series[s].data[j].x;
-                newData.y = chart.series[s3].data[i].y;
-                newData.category = chart.series[s3].data[i].category;
-                newData.drilldown = chart.series[s3].data[i].drilldown;
-                newData.name = chart.series[s3].data[i].name;
-                newData.freq = chart.series[s3].data[i].freq;
-                newData.tooltip = chart.series[s3].data[i].tooltip;
-                chart.series[s3].data[i].update(newData, false);
-                found = true;
-            }
-            else {
-                j++;
-            }
-        }
-
-        var currentIndex = chart.series[s4].data[i].index;
-        var found = false;
-        var j = 0;
-        while (found == false) {
-            if (chart.series[s].data[j].index == currentIndex) {
-                newData.index = i;
-                newData.x = chart.series[s].data[j].x;
-                newData.y = chart.series[s4].data[i].y;
-                newData.category = chart.series[s4].data[i].category;
-                newData.drilldown = chart.series[s4].data[i].drilldown;
-                newData.name = chart.series[s4].data[i].name;
-                newData.freq = chart.series[s4].data[i].freq;
-                newData.tooltip = chart.series[s4].data[i].tooltip;
-                chart.series[s4].data[i].update(newData, false);
                 found = true;
             }
             else {
