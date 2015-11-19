519b91a06a2a8b66b0c97cc9cba232456be6f159
dplatten <dplatten@hotmail.com>
2015-11-19 10:29:12 +0000

    Removed ugly code that was being used to obtain modality type. Not needed anymore. References issue #312.

--- openrem/remapp/views.py
+++ openrem/remapp/views.py
@@ -1480,17 +1480,10 @@ def charts_off(request):
 @login_required
 def display_names_view(request):
     from django.db.models import Q
-    from remapp.models import UniqueEquipmentNames, GeneralEquipmentModuleAttr, GeneralStudyModuleAttr
+    from remapp.models import UniqueEquipmentNames
     import pkg_resources # part of setuptools
 
     f = UniqueEquipmentNames.objects.order_by('display_name')
-    for item in f:
-        g = GeneralEquipmentModuleAttr.objects.filter(unique_equipment_name = item.pk)
-        h = g.values("general_study_module_attributes")
-        if len(h) > 0:
-            item.modality_type = GeneralStudyModuleAttr.objects.filter(id = h[0].values()[0]).values('modality_type')[0].values()[0]
-        else:
-            item.modality_type = False
 
     ct_names = f.filter(generalequipmentmoduleattr__general_study_module_attributes__modality_type="CT").distinct()
     mg_names = f.filter(generalequipmentmoduleattr__general_study_module_attributes__modality_type="MG").distinct()
