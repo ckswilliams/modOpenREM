97196d1d1df171c7251423ba10a2bc8cdc0b28f5
Ed McDonagh <ed@mcdonagh.org.uk>
2015-11-26 18:30:27 +0000

    Enabled crispy forms for the QR, already much nicer! Refs #316

--- openrem/remapp/forms.py
+++ openrem/remapp/forms.py
@@ -180,7 +180,10 @@ class DicomQueryForm(forms.Form):
         from remapp.models import DicomRemoteQR, DicomStoreSCP
         self.fields['remote_host_field'].choices = [(x.pk, x.name) for x in DicomRemoteQR.objects.all()]
         self.fields['store_scp_field'].choices = [(x.pk, x.name) for x in DicomStoreSCP.objects.all()]
-
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'q_process'
+        self.helper.add_input(Submit('submit', 'Submit'))
 
 class DicomDeleteSettingsForm(forms.ModelForm):
     """Form for configuring whether DICOM objects are stored or deleted once processed
