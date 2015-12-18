00d658bb94f80e3389e5aabba6758664105cd6ff
Ed McDonagh <ed@mcdonagh.org.uk>
2015-12-18 13:29:17 +0000

    keep_alive and echoscu now log to the new logs. Fixes #335. qrscu, movescu and keep_alive tasks are now explicitly named fixing #334

--- openrem/remapp/netdicom/keepalive.py
+++ openrem/remapp/netdicom/keepalive.py
@@ -1,8 +1,9 @@
 import logging
 from celery import shared_task
 
+logger = logging.getLogger(__name__)
 
-@shared_task
+@shared_task(name='remapp.netdicom.keepalive.keep_alive')
 def keep_alive():
     from remapp.models import DicomStoreSCP
     from remapp.netdicom.storescp import web_store
@@ -11,9 +12,9 @@ def keep_alive():
     for store in stores:
         if store.keep_alive:
             echo = echoscu(scp_pk=store.pk, store_scp=True)
-            logging.info("Keep_alive echo for {0} is {1}".format(store.aetitle, echo))
+            logger.debug("Keep_alive echo for {0} is {1}".format(store.aetitle, echo))
             if echo is "AssocFail":
-                logging.warning("Starting {0} on port {1} due to Association Request failure.".format(store.aetitle, store.port))
+                logger.warning("Starting {0} on port {1} due to Association Request failure.".format(store.aetitle, store.port))
                 store.status = "Store not running, but keep-alive is set!"
                 store.save()
                 web_store(store_pk=store.pk)
