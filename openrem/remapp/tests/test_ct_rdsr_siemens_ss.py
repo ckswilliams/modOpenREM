2834e2e1d3e1f065e450aa1addf0cf2ec721f31f
Ed McDonagh <ed@openrem.org>
2016-08-25 08:40:49 +0100

    Added in tests for patient ID being stored and being stored hashed. Will probably break this into a new file. Refs  #427

--- openrem/remapp/tests/test_ct_rdsr_siemens_ss.py
+++ openrem/remapp/tests/test_ct_rdsr_siemens_ss.py
@@ -1,13 +1,9 @@
 # test_get_values.py
 
+import os
 from django.test import TestCase
-from dicom.sequence import Sequence
-from dicom.dataset import Dataset
-from remapp.tools.get_values import get_seq_code_value, get_seq_code_meaning
-
-
 from remapp.extractors import rdsr
-from remapp.models import GeneralStudyModuleAttr, PatientIDSettings, DicomDeleteSettings
+from remapp.models import GeneralStudyModuleAttr, PatientIDSettings
 
 
 
@@ -19,14 +15,76 @@ class ImportCTRDSR(TestCase):
         """
         PatientIDSettings.objects.create()
 
-        import os
         dicom_file = "test_files/CT-RDSR-Siemens_Flash-TAP-SS.dcm"
         root_tests = os.path.dirname(os.path.abspath(__file__))
         dicom_path = os.path.join(root_tests, dicom_file)
 
         rdsr(dicom_path)
-        a = GeneralStudyModuleAttr.objects.all()
-        b = a[0]
+        study = GeneralStudyModuleAttr.objects.all()[0]
 
-        self.assertEqual(b.accession_number, 'ACC12345601')
+        # Test that patient identifiable data is not stored
+        self.assertEqual(study.patientmoduleattr_set.get().patient_name, None)
 
+        # Test that study level data is recorded correctly
+        self.assertEqual(study.accession_number, 'ACC12345601')
+
+
+    def test_import_with_ids(self):
+        """
+        Imports a known RDSR file derived from a Siemens Definition Flash and tests that patient IDs are stored when
+        requested.
+        """
+        import datetime
+        pid = PatientIDSettings.objects.create()
+        pid.name_stored = True
+        pid.name_hashed = False
+        pid.id_stored = True
+        pid.id_hashed = False
+        pid.dob_stored = True
+        pid.save()
+        self.assertEqual(pid.name_stored, True)
+
+        dicom_file = "test_files/CT-RDSR-Siemens_Flash-TAP-SS.dcm"
+        root_tests = os.path.dirname(os.path.abspath(__file__))
+        dicom_path = os.path.join(root_tests, dicom_file)
+
+        rdsr(dicom_path)
+        study = GeneralStudyModuleAttr.objects.all()[0]
+
+        # Test that patient identifiable data is stored in plain text
+        self.assertEqual(study.patientmoduleattr_set.get().patient_name, 'SMITH^JOHN')
+        self.assertEqual(study.patientmoduleattr_set.get().patient_id, '123456')
+        dob = datetime.date(1929, 5, 19)
+        self.assertEqual(study.patientmoduleattr_set.get().patient_birth_date, dob)
+
+
+    def test_import_with_ids_hashed(self):
+        """
+        Imports a known RDSR file derived from a Siemens Definition Flash and tests that patient IDs are stored when
+        requested, in the form of a hash
+        """
+        import hashlib
+        pid = PatientIDSettings.objects.create()
+        pid.name_stored = True
+        pid.name_hashed = True
+        pid.id_stored = True
+        pid.id_hashed = True
+        pid.accession_hashed = True
+        pid.save()
+        self.assertEqual(pid.name_stored, True)
+
+        dicom_file = "test_files/CT-RDSR-Siemens_Flash-TAP-SS.dcm"
+        root_tests = os.path.dirname(os.path.abspath(__file__))
+        dicom_path = os.path.join(root_tests, dicom_file)
+
+        rdsr(dicom_path)
+        study = GeneralStudyModuleAttr.objects.all()[0]
+
+        name = hashlib.sha256('SMITH^JOHN').hexdigest()
+        pat_id = hashlib.sha256('123456').hexdigest()
+        acc = hashlib.sha256('ACC12345601').hexdigest()
+
+        # Test that patient identifiable data is stored in plain text
+        self.assertEqual(study.patientmoduleattr_set.get().patient_name, name)
+        self.assertEqual(study.patientmoduleattr_set.get().patient_id, pat_id)
+        self.assertEqual(study.accession_number, acc)
