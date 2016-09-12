b4de7280afbe5a228ef7327e840ce19e8ebaba85
Ed McDonagh <ed@openrem.org>
2016-09-12 10:03:48 +0100

    Adding some test code for the filter materials extraction for DX. Refs #431 and pull request #53

---
+++ openrem/remapp/tests/test_dx_imports.py
@@ -0,0 +1,32 @@
+# This Python file uses the following encoding: utf-8
+# test_get_values.py
+
+from __future__ import unicode_literals
+from django.test import TestCase
+from dicom.sequence import Sequence
+from dicom.dataset import Dataset
+from remapp.extractors.dx import _xray_filters_multiple
+from remapp.models import GeneralStudyModuleAttr, ProjectionXRayRadiationDose, IrradEventXRayData, \
+    IrradEventXRaySourceData
+
+class DXImportTests(TestCase):
+    def test_multiple_filter_kodak_dr7500(self):
+        """
+        """
+        ds = Dataset()
+        FilterMaterial = "aluminium,copper"
+        ds.FilterThicknessMinimum = "1.0\\0.1"
+        ds.FilterThicknessMaximum = "1.0\\0.1"
+
+        g = GeneralStudyModuleAttr.objects.create()
+        g.save()
+        proj = ProjectionXRayRadiationDose.objects.create(general_study_module_attributes=g)
+        proj.save()
+        event = IrradEventXRayData.objects.create(projection_xray_radiation_dose=proj)
+        event.save()
+        source = IrradEventXRaySourceData.objects.create(irradiation_event_xray_data=event)
+        source.save()
+
+        _xray_filters_multiple(FilterMaterial, ds.FilterThicknessMaximum, ds.FilterThicknessMinimum, source)
+
+        self.assertEqual(source.xrayfilters_set.all().count(), 2)
