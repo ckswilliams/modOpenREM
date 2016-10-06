8707d141de6b64c79126f88bcceff2dbfd1c6be8
Ed McDonagh <ed@openrem.org>
2016-10-06 08:47:22 +0100

    Adding start of MG import tests. Currently fails.

---
+++ openrem/remapp/tests/test_import_mam.py
@@ -0,0 +1,45 @@
+# This Python file uses the following encoding: utf-8
+# test_get_values.py
+
+import os
+from decimal import Decimal
+from django.test import TestCase
+from remapp.extractors import mam
+from remapp.models import GeneralStudyModuleAttr, PatientIDSettings
+
+
+
+class ImportMGImg(TestCase):
+    def test_import_mg_img_ge(self):
+        """
+        Imports a known mammography image file derived from a GE Senographe DS image, and tests the values
+        imported against those expected.
+        """
+        PatientIDSettings.objects.create()
+
+        dicom_file = "test_files/MG-Im-GE-SenDS-scaled.dcm"
+        root_tests = os.path.dirname(os.path.abspath(__file__))
+        dicom_path = os.path.join(root_tests, dicom_file)
+
+        mam(dicom_path)
+        study = GeneralStudyModuleAttr.objects.all()[0]
+
+        # Test that patient identifiable data is not stored
+        self.assertEqual(study.patientmoduleattr_set.get().patient_name, None)
+
+        # Test that study level data is recorded correctly
+        self.assertEqual(study.accession_number, 'AAAA9876')
+        self.assertEqual(study.generalequipmentmoduleattr_set.get().institution_name, u'中心医院')
+        self.assertEqual(study.generalequipmentmoduleattr_set.get().manufacturer, 'GE MEDICAL SYSTEMS')
+
+        # Test that patient study level data is recorded correctly
+        self.assertEqual(study.patientstudymoduleattr_set.get().patient_age, '001D')
+        self.assertAlmostEqual(study.patientstudymoduleattr_set.get().patient_age_decimal, Decimal(67.6))
+
+        # Test that exposure data is recorded correctly
+        self.assertEqual(study.ctradiationdose_set.get().ctaccumulateddosedata_set.get().
+                         total_number_of_irradiation_events, 4)
+        self.assertAlmostEqual(study.ctradiationdose_set.get().ctaccumulateddosedata_set.get().
+                         ct_dose_length_product_total, Decimal(724.52))
+
+
