0079f3ac4d3ca79876b814d4cbd3e9f16091cb5b
Ed McDonagh <ed@openrem.org>
2016-10-10 17:42:22 +0100

    Added hashed version of mammo import ID check. UFT-8 strings need to be bytecode before hashing. Need to fix on other extractors too. Refs #427, #434

--- openrem/remapp/tests/test_import_mam.py
+++ openrem/remapp/tests/test_import_mam.py
@@ -47,6 +47,7 @@ class ImportMGImg(TestCase):
         Imports a known mammography image file derived from a GE Senographe DS image, and tests the values
         imported against those expected.
         """
+        import datetime
         pid = PatientIDSettings.objects.create()
         pid.name_stored = True
         pid.name_hashed = False
@@ -65,3 +66,31 @@ class ImportMGImg(TestCase):
         # Test that patient identifiable data is stored in plain text
         self.assertEqual(study.patientmoduleattr_set.get().patient_name, u'Mamografía^Bịnhnhân')
         self.assertEqual(study.patientmoduleattr_set.get().patient_id, u'ABCD1234')
+        birth_date = datetime.date(2013, 4, 12)
+        self.assertEqual(study.patientmoduleattr_set.get().patient_birth_date, birth_date)
+
+
+    def test_import_mg_img_ge_pid_hashed(self):
+        """
+        Imports a known mammography image file derived from a GE Senographe DS image, and tests the values
+        imported against those expected.
+        """
+        pid = PatientIDSettings.objects.create()
+        pid.name_stored = True
+        pid.name_hashed = True
+        pid.id_stored = True
+        pid.id_hashed = True
+        pid.save()
+
+        dicom_file = "test_files/MG-Im-GE-SenDS-scaled.dcm"
+        root_tests = os.path.dirname(os.path.abspath(__file__))
+        dicom_path = os.path.join(root_tests, dicom_file)
+
+        mam(dicom_path)
+        study = GeneralStudyModuleAttr.objects.all()[0]
+
+        # Test that patient identifiable data is stored in plain text
+        self.assertEqual(study.patientmoduleattr_set.get().patient_name,
+                         'fe66aceb4eb0ccbd76306a485e162cc3cad8c9312b25b002ad784f72575ae500')
+        self.assertEqual(study.patientmoduleattr_set.get().patient_id,
+                         '1635c8525afbae58c37bede3c9440844e9143727cc7c160bed665ec378d8a262')
