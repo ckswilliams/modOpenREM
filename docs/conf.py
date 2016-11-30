bbd0bd282215a7ffebc530a67a72c6ec38765a87
Ed McDonagh <ed@mcdonagh.org.uk>
2016-11-30 15:18:01 +0000

    Enabling graphviz

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'celery.contrib.sphinx',
     'sphinx.ext.napoleon',
+    'sphinx.ext.graphviz',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
