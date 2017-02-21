1f11c4b11f5f88e7705cd0874b588ae8988799f4
Ed McDonagh <ed@openrem.org>
2017-02-21 18:10:45 +0000

    Testing to ref #470 and test pull request #62. Also refs #473

--- openrem/remapp/tests/test_dicom_qr.py
+++ openrem/remapp/tests/test_dicom_qr.py
@@ -9,10 +9,8 @@ from remapp.models import DicomQuery, DicomQRRspStudy, DicomQRRspSeries
 
 
 class DicomQR(TestCase):
-    def test_response_sorting_ct_philips_with_desc(self):
+    def setUp(self):
         """
-        Imports a known RDSR file derived from a Siemens Definition Flash and tests that patient IDs are stored when
-        requested.
         """
 
         query = DicomQuery.objects.create()
@@ -57,11 +55,19 @@ class DicomQR(TestCase):
         rst1.set_modalities_in_study(list(set(val for dic in rst1_series_rsp.values('modality') for val in dic.values())))
         rst1.save()
 
+    def test_response_sorting_ct_philips_with_desc(self):
+        """
+        Study response contains a Philips style 'dose info' series, with study descriptions available, and no structured
+        report series. Expect a single series to be left after pruning.
+        """
         all_mods = {'CT': {'inc': True, 'mods': ['CT']},
                     'MG': {'inc': False, 'mods': ['MG']},
                     'FL': {'inc': False, 'mods': ['RF', 'XA']},
                     'DX': {'inc': False, 'mods': ['DX', 'CR']}
                     }
+
+        query = DicomQuery.objects.get()
+        rst1 = query.dicomqrrspstudy_set.all()[0]
 
         self.assertEqual(rst1.dicomqrrspseries_set.all().count(), 3)
 
@@ -70,3 +76,37 @@ class DicomQR(TestCase):
         self.assertEqual(query.dicomqrrspstudy_set.all().count(), 1)
         self.assertEqual(rst1.dicomqrrspseries_set.all().count(), 1)
         self.assertEqual(rst1.dicomqrrspseries_set.all()[0].series_description, u"dose info")
+
+    def test_response_sorting_ct_philips_no_desc(self):
+        """
+        Study response contains a Philips style 'dose info' series, but without study descriptions available, and no
+        structured report series. Expect two series to be left after pruning, with the main series removed.
+        """
+        all_mods = {'CT': {'inc': True, 'mods': ['CT']},
+                    'MG': {'inc': False, 'mods': ['MG']},
+                    'FL': {'inc': False, 'mods': ['RF', 'XA']},
+                    'DX': {'inc': False, 'mods': ['DX', 'CR']}
+                    }
+
+        query = DicomQuery.objects.get()
+        rst1 = query.dicomqrrspstudy_set.all()[0]
+
+        rst1_series_rsp = rst1.dicomqrrspseries_set.all()
+        rst1s1 = rst1_series_rsp[0]
+        rst1s2 = rst1_series_rsp[1]
+        rst1s3 = rst1_series_rsp[2]
+        rst1s1.series_description = None
+        rst1s2.series_description = None
+        rst1s3.series_description = None
+        rst1s1.save()
+        rst1s2.save()
+        rst1s3.save()
+
+        # Before pruning, three series
+        self.assertEqual(rst1.dicomqrrspseries_set.all().count(), 3)
+
+        qrscu._prune_series_responses(query, all_mods)
+
+        # After pruning, two series
+        self.assertEqual(query.dicomqrrspstudy_set.all().count(), 1)
+        self.assertEqual(rst1.dicomqrrspseries_set.all().count(), 2)
