b0b1f9d230218aba8f058f0c05e771223a41790a
Ed McDonagh <ed@mcdonagh.org.uk>
2017-11-10 23:44:24 +0000

    Excluding tasks.py imports from pylint checking

--- openrem/remapp/tasks.py
+++ openrem/remapp/tasks.py
@@ -1,18 +1,17 @@
 from __future__ import absolute_import
 
-from celery import shared_task
+from celery import shared_task  # pylint: disable=unused-import
 
-
-from remapp.exports.exportcsv import exportMG2excel
-from remapp.exports.ct_export import ctxlsx, ct_csv
-from remapp.exports.mg_csv_nhsbsp import mg_csv_nhsbsp
-from remapp.extractors.ptsizecsv2db import websizeimport
-from remapp.exports.dx_export import exportDX2excel, dxxlsx
-from remapp.exports.rf_export import exportFL2excel, rfxlsx
-from remapp.extractors.dx import dx
-from remapp.extractors.rdsr import rdsr
-from remapp.extractors.mam import mam
-from remapp.extractors.ct_philips import ct_philips
-from remapp.netdicom.qrscu import qrscu, movescu
-from remapp.netdicom.keepalive import keep_alive
-from remapp.tools.make_skin_map import make_skin_map
+from remapp.exports.exportcsv import exportMG2excel  # pylint: disable=unused-import
+from remapp.exports.ct_export import ctxlsx, ct_csv  # pylint: disable=unused-import
+from remapp.exports.mg_csv_nhsbsp import mg_csv_nhsbsp  # pylint: disable=unused-import
+from remapp.extractors.ptsizecsv2db import websizeimport  # pylint: disable=unused-import
+from remapp.exports.dx_export import exportDX2excel, dxxlsx  # pylint: disable=unused-import
+from remapp.exports.rf_export import exportFL2excel, rfxlsx  # pylint: disable=unused-import
+from remapp.extractors.dx import dx  # pylint: disable=unused-import
+from remapp.extractors.rdsr import rdsr  # pylint: disable=unused-import
+from remapp.extractors.mam import mam  # pylint: disable=unused-import
+from remapp.extractors.ct_philips import ct_philips  # pylint: disable=unused-import
+from remapp.netdicom.qrscu import qrscu, movescu  # pylint: disable=unused-import
+from remapp.netdicom.keepalive import keep_alive  # pylint: disable=unused-import
+from remapp.tools.make_skin_map import make_skin_map  # pylint: disable=unused-import
