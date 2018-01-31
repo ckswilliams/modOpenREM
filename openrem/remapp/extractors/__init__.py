7b0d09947c8a48e12109dbe9e4ff7da33fb7730d
Ed McDonagh <ed@mcdonagh.org.uk>
2018-01-31 00:00:39 +0000

    Adding ref #579 to changes. Reverted __init__.py to original.

--- openrem/remapp/extractors/__init__.py
+++ openrem/remapp/extractors/__init__.py
@@ -3,4 +3,4 @@ from mam import mam
 from dx import dx
 from ct_philips import ct_philips
 from rdsr_toshiba_ct_from_dose_images import rdsr_toshiba_ct_from_dose_images
-# #from openrem_settings import openrem_settings
+#from openrem_settings import openrem_settings
