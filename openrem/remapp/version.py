d8e1c7c5525d1d105a6919dc991e2e77181fc442
Ed McDonagh <ed@mcdonagh.org.uk>
2018-02-05 08:27:27 +0000

    Updating docs version for 0.8.0b1

--- openrem/remapp/version.py
+++ openrem/remapp/version.py
@@ -1,5 +1,5 @@
 __version__ = "0.8.0b1"
-__docs_version__ = "latest"
+__docs_version__ = "0.8.0b1"
 __short_version__ = "0.8"
 __skin_map_version__ = "0.7"
 __netdicom_implementation_version__ = "0.8.0.1"
