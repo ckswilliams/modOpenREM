6c2a0a996f95f6462b88ed930c0b2b3ca34ba919
Ed McDonagh <ed@mcdonagh.org.uk>
2018-05-29 22:06:41 +0100

    Now drops any non SR/CT/MG/CR/DX modality objects, and processes what's left against the appropriate import script. Doesn't cater for Toshiba CT. Refs #635

--- stuff/openrem.lua
+++ stuff/openrem.lua
@@ -1,27 +1,49 @@
 local python_path = '/home/mcdonaghe/research/veOpenREM/bin/python'
 local openrem_exe_path = '/home/mcdonaghe/research/veOpenREM/bin/'
+local temp_path = '/tmp/'
+
+function ReceivedInstanceFilter(dicom, origin)
+    -- Only allow incoming objects we can use
+    local mod = dicom.Modality
+    if (mod ~= 'SR') and (mod ~= 'CT') and (mod ~= 'MG') and (mod ~= 'CR') and (mod ~= 'DX') then
+        return false
+    else
+        return true
+    end
+end
 
 function OnStoredInstance(instanceId, tags)
     -- Retrieve the DICOM instance from Orthanc
     local dicom = RestApiGet('/instances/' .. instanceId .. '/file')
 
     -- Work out if file can be used, if not delete from Orthanc?
-
-    -- assume at this point we have RDSR
-    local import_script = 'openrem_rdsr.py'
+    local import_script = ''
+    if tags['SOPClassUID'] == '1.2.840.10008.5.1.4.1.1.88.67' then
+        import_script = 'openrem_rdsr.py'
+    elseif tags['SOPClassUID'] == '1.2.840.10008.5.1.4.1.1.88.22' then
+        -- Enhanced SR used by GE CT Scanners
+        import_script = 'openrem_rdsr.py'
+    elseif tags['Modality'] == 'MG' then
+        import_script = 'openrem_mg.py'
+    elseif (tags['Modality'] == 'CR') or (tags['Modality'] == 'DX') then
+        import_script = 'openrem_dx.py'
+    elseif tags['SOPClassUID'] == '1.2.840.10008.5.1.4.1.1.7' then
+        -- Secondary Capture object that might be Philips CT Dose Info image
+        import_script = 'openrem_ctphilips.py'
+    end
 
     -- Write the DICOM content to some temporary file
-    local path = instanceId .. '.dcm'
-    local target = assert(io.open('/tmp/' .. path, 'wb'))
+    local path = temp_path .. instanceId .. '.dcm'
+    local target = assert(io.open(path, 'wb'))
     target:write(dicom)
     target:close()
 
     -- Call OpenREM import script
     -- Runs as orthanc user in linux, so log files must be writable by orthanc
-    os.execute(python_path .. ' ' .. openrem_exe_path .. import_script .. ' ' .. '/tmp/' .. path)
+    os.execute(python_path .. ' ' .. openrem_exe_path .. import_script .. ' ' .. path)
 
     -- Remove the temporary DICOM file
-    os.remove('/tmp/' .. path)
+    os.remove(path)
 
     -- Remove study from Orthanc
     Delete(instanceId)
