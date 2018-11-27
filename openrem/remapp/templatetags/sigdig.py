6452d23227a77e08931f3f11974dcac5ca8af1db
Ed McDonagh <ed@mcdonagh.org.uk>
2018-11-27 20:34:39 +0000

    Added filter snippet to set significant figures. From https://djangosnippets.org/snippets/1372/ but edited to deal with "0E-12" and to return a Decimal that can be localised. Refs #693

---
+++ openrem/remapp/templatetags/sigdig.py
@@ -0,0 +1,21 @@
+# This Python file uses the following encoding: utf-8
+
+from django import template
+from decimal import Decimal
+import math
+
+register = template.Library()
+
+
+@register.filter
+def sigdig(value, digits=3):
+    if value > Decimal(0):
+        order = int(math.floor(math.log10(math.fabs(value))))
+        places = digits - order - 1
+        if places > 0:
+            fmtstr = "%%.%df" % (places)
+        else:
+            fmtstr = "%.0f"
+        return Decimal(fmtstr % (round(value, places)))
+    else:
+        return '0'
