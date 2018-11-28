52a3e79d24a9f65fdf4188d33b08abcb57d22706
Ed McDonagh <ed@mcdonagh.org.uk>
2018-11-28 21:45:08 +0000

    Adding module and function docs strings. Refs #693

--- openrem/remapp/templatetags/sigdig.py
+++ openrem/remapp/templatetags/sigdig.py
@@ -1,5 +1,12 @@
 # This Python file uses the following encoding: utf-8
 
+"""
+..  module:: sigdig
+    :synopsis: template filter for returning number with three or specified number of significant digits
+
+    Modified from Django Snippet at https://djangosnippets.org/snippets/1372/
+"""
+
 from django import template
 from decimal import Decimal
 import math
@@ -9,6 +16,13 @@ register = template.Library()
 
 @register.filter
 def sigdig(value, digits=3):
+    """ Template filter to return number with specified number of significant figures
+
+    :param value: number to consider
+    :param digits: number of significant figures, default 3
+    :return: number to specified number of significant figures
+    """
+
     if value is None:
         return ''
     if value != Decimal(0):
