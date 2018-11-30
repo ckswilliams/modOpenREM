eb6dc86d0953f32ad10220b5c1313bf36c1ef746
David Platten <dplatten@gmail.com>
2018-11-30 10:40:23 +0000

    Tests for correct cumulated DAP and dose at RP; tests for correct highlighting of cumulated and non-cumulated DAP and dose at RP. References issue #697

--- openrem/remapp/tests/test_rf_high_dose_alert.py
+++ openrem/remapp/tests/test_rf_high_dose_alert.py
@@ -9,6 +9,7 @@ from remapp.models import PatientIDSettings, GeneralStudyModuleAttr, HighDoseMet
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse_lazy
 from remapp.interface.mod_filters import RFSummaryListFilter
+from decimal import Decimal
 
 
 class RFHighDoseAlert(TestCase):
@@ -39,9 +40,9 @@ class RFHighDoseAlert(TestCase):
         alert_settings.calc_accum_dose_over_delta_weeks_on_import = True
         alert_settings.save()
 
-        rf_siemens_zee_20160510 = os.path.join("test_files", "RF-RDSR-Siemens-Zee_adjusted.dcm")
         rf_siemens_zee_20160512 = os.path.join("test_files", "RF-RDSR-Siemens-Zee.dcm")
-        # The second file has had the study date adjusted by a few days, and the Study UID sdjusted by a single digit
+        rf_siemens_zee_20160510 = os.path.join("test_files", "RF-RDSR-Siemens-Zee_adjusted.dcm")
+        # The second file has had the study date adjusted by two days, and the Study UID adjusted by a single digit
 
         root_tests = os.path.dirname(os.path.abspath(__file__))
         # Important to read in the earlist study date RDSR first because
@@ -51,7 +52,7 @@ class RFHighDoseAlert(TestCase):
         rdsr(os.path.join(root_tests, rf_siemens_zee_20160512))
 
     def test_cumulative_dap(self):
-        """ Test that the calculated cumulative DAP over delta weeks is correct
+        """ Test that the calculated cumulative DAP over delta weeks is correct for the two studies
         """
         self.client.login(username='temporary', password='temporary')
         filter_set = ''
@@ -60,12 +61,12 @@ class RFHighDoseAlert(TestCase):
         # Test that cumulative DAP matches what I expect below
         # Using AlmostEqual as comparing floating point numbers
         total_dap_over_week_delta = f.qs.values_list('projectionxrayradiationdose__accumxraydose__accumintegratedprojradiogdose__dose_area_product_total_over_delta_weeks', flat=True)[0]
-        expected_value = 0.000032000000
-        self.assertAlmostEqual(total_dap_over_week_delta, expected_value, places=7, msg=None, delta=None)
+        expected_value = Decimal(0.0000320)
+        self.assertAlmostEqual(total_dap_over_week_delta, expected_value, places=7, msg=None)
 
         total_dap_over_week_delta = f.qs.values_list('projectionxrayradiationdose__accumxraydose__accumintegratedprojradiogdose__dose_area_product_total_over_delta_weeks', flat=True)[1]
-        expected_value = 0.000016000000
-        self.assertAlmostEqual(total_dap_over_week_delta, expected_value, places=7, msg=None, delta=None)
+        expected_value = Decimal(0.0000160)
+        self.assertAlmostEqual(total_dap_over_week_delta, expected_value, places=7, msg=None)
 
     def test_cumulative_rp_dose(self):
         """ Test that calculated cumulative total dose at RP over delta weeks is correct
@@ -77,12 +78,12 @@ class RFHighDoseAlert(TestCase):
         # Test that cumulative DAP matches what I expect below
         # Using AlmostEqual as comparing floating point numbers
         total_rp_dose_over_week_delta = f.qs.values_list('projectionxrayradiationdose__accumxraydose__accumintegratedprojradiogdose__dose_rp_total_over_delta_weeks', flat=True)[0]
-        expected_value = 0.005040000000
-        self.assertAlmostEqual(total_rp_dose_over_week_delta, expected_value, places=7, msg=None, delta=None)
+        expected_value = Decimal(0.0050400)
+        self.assertAlmostEqual(total_rp_dose_over_week_delta, expected_value, places=7, msg=None)
 
         total_rp_dose_over_week_delta = f.qs.values_list('projectionxrayradiationdose__accumxraydose__accumintegratedprojradiogdose__dose_rp_total_over_delta_weeks', flat=True)[1]
-        expected_value = 0.002520000000
-        self.assertAlmostEqual(total_rp_dose_over_week_delta, expected_value, places=7, msg=None, delta=None)
+        expected_value = Decimal(0.0025200)
+        self.assertAlmostEqual(total_rp_dose_over_week_delta, expected_value, places=7, msg=None)
 
     def test_dap_alert(self):
         """Test that DAP alert highlights the value in red appropriately
@@ -90,35 +91,55 @@ class RFHighDoseAlert(TestCase):
         """
         self.client.login(username='temporary', password='temporary')
 
+        # First DAP alert level tests
         # Second study should alert on cumulative DAP
         alert_settings = HighDoseMetricAlertSettings.objects.all()[0]
         alert_settings.alert_total_dap_rf = 30.0
         alert_settings.show_accum_dose_over_delta_weeks = True
         alert_settings.save()
+
         # Recalculate the cumulative dose data because DAP alert changed
         self.client.get(reverse_lazy('rf_recalculate_accum_doses'), follow=True)
-        # See if the alert highlighting works
+
+        # Obtain the response from the RF summary list filter - this includes the html of the page
         response = self.client.get(reverse_lazy('rf_summary_list_filter'), follow=True)
+
         # One study should be highlighted
         response_text = u'<strong style="color: red;">32.0</strong>'
-        self.assertContains(response, response_text)
+        self.assertContains(response, response_text, count=1)
+
         # The other should not
         response_text = u'<strong style="color: red;">16.0</strong>'
         self.assertNotContains(response, response_text)
 
+        # The string (2 exams) should appear twice for the more recent study (once in the DAP field, once in the RP field)
+        response_text = u'(2 exams)'
+        self.assertContains(response, response_text, count=2)
+
+        # The string (1 exam) should appear twice for the earlier study (once in the DAP field, once in the RP field)
+        response_text = u'(1 exam)'
+        self.assertContains(response, response_text, count=2)
+
+        # Second DAP alert level tests
         # Both studies should alert on total DAP; second should alert on cumulated DAP
         alert_settings.alert_total_dap_rf = 15.0
         alert_settings.show_accum_dose_over_delta_weeks = True
         alert_settings.save()
+
         # Recalculate the cumulative dose data because DAP alert changed
         self.client.get(reverse_lazy('rf_recalculate_accum_doses'), follow=True)
-        # See if the alert highlighting works
+
+        # Obtain the response from the RF summary list filter - this includes the html of the page
         response = self.client.get(reverse_lazy('rf_summary_list_filter'), follow=True)
-        # Both studies should be highlighted
+
+        # The latest study should have cumulative DAP highlighted
         response_text = u'<strong style="color: red;">32.0</strong>'
-        self.assertContains(response, response_text)
+        self.assertContains(response, response_text, count=1)
+
+        # The latest study should have 16.0 cGy.cm^2 total DAP highlighted, and the earlier study should have
+        # 16.0 cGy.cm^2 total DAP and cumulative DAP highlighted
         response_text = u'<strong style="color: red;">16.0</strong>'
-        self.assertContains(response, response_text)
+        self.assertContains(response, response_text, count=3)
 
     def test_rp_dose_alert(self):
         """Test that dose at RP alert highlights the value in red appropriately
@@ -126,32 +147,44 @@ class RFHighDoseAlert(TestCase):
         """
         self.client.login(username='temporary', password='temporary')
 
+        # First dose at RP test
         # Second study should alert on RP dose
         alert_settings = HighDoseMetricAlertSettings.objects.all()[0]
-        alert_settings.alert_total_rp_dose_rf = 0.005000000000
+        alert_settings.alert_total_rp_dose_rf = 0.0050
         alert_settings.show_accum_dose_over_delta_weeks = True
         alert_settings.save()
+
         # Recalculate the cumulative dose data because DAP alert changed
         self.client.get(reverse_lazy('rf_recalculate_accum_doses'), follow=True)
-        # See if the alert highlighting works
+
+        # Obtain the response from the RF summary list filter - this includes the html of the page
         response = self.client.get(reverse_lazy('rf_summary_list_filter'), follow=True)
-        # One study should be highlighted
-        response_text = u'<strong style="color: red;">32.0</strong>'
-        self.assertContains(response, response_text)
-        # The other should not
-        response_text = u'<strong style="color: red;">16.0</strong>'
+
+        # Cumulative RP dose should be highlighted for the latest study
+        response_text = u'<strong style="color: red;"><script>document.write(0.005040000000.toPrecision(3));</script></strong>'
+        self.assertContains(response, response_text, count=1)
+
+        # Cumulative RP dose should not be highlighted for the earlier study, and nor should the total dose at RP
+        response_text = u'<strong style="color: red;"><script>document.write(0.002520000000.toPrecision(3));</script></strong>'
         self.assertNotContains(response, response_text)
 
+        # Second dose at RP test
         # Both studies should alert on RP dose ; second should alert on cumulated RP dose
-        alert_settings.alert_total_rp_dose_rf = 0.002500000000
+        alert_settings.alert_total_rp_dose_rf = 0.0025
         alert_settings.show_accum_dose_over_delta_weeks = True
         alert_settings.save()
+
         # Recalculate the cumulative dose data because DAP alert changed
         self.client.get(reverse_lazy('rf_recalculate_accum_doses'), follow=True)
-        # See if the alert highlighting works
+
+        # Obtain the response from the RF summary list filter - this includes the html of the page
         response = self.client.get(reverse_lazy('rf_summary_list_filter'), follow=True)
-        # Both studies should be highlighted
-        response_text = u'<strong style="color: red;">32.0</strong>'
-        self.assertContains(response, response_text)
-        response_text = u'<strong style="color: red;">16.0</strong>'
-        self.assertContains(response, response_text)
+
+        # Cumulative RP dose should be highlighted for the latest study
+        response_text = u'<strong style="color: red;"><script>document.write(0.005040000000.toPrecision(3));</script></strong>'
+        self.assertContains(response, response_text, count=1)
+
+        # The latest study should have total RP dose highlighted, and the earlier study should have
+        # total RP dose and cumulative RP dose highlighted
+        response_text = u'<strong style="color: red;"><script>document.write(0.002520000000.toPrecision(3));</script></strong>'
+        self.assertContains(response, response_text, count=3)
