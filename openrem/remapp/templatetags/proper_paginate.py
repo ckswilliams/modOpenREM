295a9be5cf930ccf7efe807ceacf242a204a7bbc
Ed McDonagh <ed@mcdonagh.org.uk>
2019-04-15 22:21:16 +0100

    Added doc strings to satisfy Codacy. Also reduced neighbours to 5 each side. Refs #404

--- openrem/remapp/templatetags/proper_paginate.py
+++ openrem/remapp/templatetags/proper_paginate.py
@@ -1,11 +1,24 @@
+"""
+..  module:: proper_paginate
+    :synoposis: Allows sensible list of pages to click on when paginating over large numbers of pages
+
+..  moduleauthor:: see https://gist.github.com/sumitlni/4f308e5999d2d4d8cb284fea7bf0309c
+"""
+
 from django import template
 
 register = template.Library()
 
 
-# From https://gist.github.com/sumitlni/4f308e5999d2d4d8cb284fea7bf0309c
 @register.filter(name='proper_paginate')
-def proper_paginate(paginator, current_page, neighbors=10):
+def proper_paginate(paginator, current_page, neighbors=5):
+    """Paginate over sensible number of page links
+
+    :param paginator: paginator pages
+    :param current_page: current page number
+    :param neighbors: how many page links to display
+    :return: range of page links to display
+    """
     print(paginator)
     if paginator.num_pages > 2*neighbors:
         start_index = max(1, current_page-neighbors)
